c47c390b87cfd9bae390f0afe2ac797ffeecc47a
Monica <a01707439@itesm.mx>
2023-03-30 15:27:26 -0600

    [INFRA] Fixed ports and Set-Location path

--- dev.ps1
+++ dev.ps1
@@ -1,3 +1,6 @@
+$ScriptPath = $MyInvocation.MyCommand.Definition
+$ScriptDirectory = Split-Path $ScriptPath
+
 Remove-Job -Name RamFrontend -Force -ErrorAction SilentlyContinue
 Remove-Job -Name RamBackend -Force -ErrorAction SilentlyContinue
 
@@ -5,22 +8,26 @@ try {
     docker build -f ram-infra/Dockerfile -t ram:local --target local .
     docker container rm -f ram-local
     docker run -d `
-        --name ram-local `
-        -p 5432:5432,9000:9000,9001:9001 `
+	    --name ram-local `
+        -p 5432:5432 `
+        -p 9000:9000 `
+        -p 9001:9001 `
         --env PGPASSWORD=password `
-        ram:local
+        ram:local 
 
     Start-Job -Name RamFrontend -ScriptBlock {
-        Set-Location ram-front
+        param ( $path )
+        Set-Location -Path "$path\ram-front"
         yarn
         yarn dev
-    }
+    } -ArgumentList $ScriptDirectory
 
-    Start-Job -Name RamBackend -ScriptBlock {
-        Set-Location ram-back
+    Start-Job -Name RamBackend -ScriptBlock{
+        param ( $path )
+	    Set-Location -Path "$path\ram-back"
         yarn
         yarn start:dev
-    }
+    } -ArgumentList $ScriptDirectory
 
 
     Start-Sleep -Seconds 3
