dbc06fae08496438b852d3275480acfa46d8efff
alfredohuerta <eagarciahuerta@hotmail.com>
2023-04-10 16:12:05 -0600

    test

--- ram-front/src/__test__/App.test.tsx
+++ ram-front/src/__test__/App.test.tsx
@@ -1,7 +1,9 @@
-import { render } from '@testing-library/react';
-import App from '../App';
+// (c) Delta Software 2023, rights reserved.
 
-test('Renders main page correctly', () => {
-    render(<App/>);
-    expect(true).toBeTruthy();
+import { render } from "@testing-library/react";
+import App from "../App";
+
+test("Renders main page correctly", () => {
+  render(<App />);
+  expect(true).toBeTruthy();
 });
