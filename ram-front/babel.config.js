c0dd352e58dc18f94c739e6af523175f02706544
alfredohuerta <eagarciahuerta@hotmail.com>
2023-04-10 17:54:12 -0600

    [INFRA] Change jest test command

--- ram-front/babel.config.js
+++ ram-front/babel.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    presets: [
-        [ '@babel/preset-env', { targets: { esmodules: true } } ],
-        [ '@babel/preset-react', { runtime: 'automatic' } ],
-        '@babel/preset-typescript',
-    ],
+  presets: [
+    ["@babel/preset-env", { targets: { esmodules: true } }],
+    ["@babel/preset-react", { runtime: "automatic" }],
+    "@babel/preset-typescript",
+  ],
 };
