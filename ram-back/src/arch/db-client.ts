f1dc709e4de794705ca556fce2c390a34b0df15d
Kenny Vercaemer <vercaemereduard@gmail.com>
2023-04-11 17:36:59 -0600

    [INFRA]: Add typeorm to backend

--- ram-back/src/arch/db-client.ts
+++ ram-back/src/arch/db-client.ts
@@ -3,26 +3,52 @@
  * This file abstracts the database connection setup away from functional code.
  */
 
-import { Client } from "pg";
+import { DataSource } from "typeorm";
 
-export function getDbClient(): Client {
+let dataSource: DataSource | null = null;
+
+export async function getDataSource(): Promise<DataSource> {
+  if (dataSource) {
+    return dataSource;
+  }
+
   if (!process.env.NODE_ENV || process.env.NODE_ENV === "remote") {
     // local and remote architectures use PG env variables directly
-    return new Client();
+    dataSource = new DataSource({
+      type: "postgres",
+      host: process.env.PGHOST,
+      port: parseInt(process.env.PGPORT || "5432"),
+      database: process.env.PGDATABASE,
+      username: process.env.PGUSER,
+      password: process.env.PGPASSWORD,
+    });
+
+    await dataSource
+      .initialize()
+      .then(() => console.info("Using local/remote DataSource connection"));
+
+    return dataSource;
   }
 
   if (process.env.NODE_ENV === "aws") {
     // AWS uses the RDS instance
-    return new Client({
+    dataSource = new DataSource({
+      type: "postgres",
       host: process.env.RDS_HOSTNAME,
       port: parseInt(process.env.RDS_PORT || "5432"),
       database: process.env.RDS_DB_NAME,
-      user: process.env.RDS_USERNAME,
+      username: process.env.RDS_USERNAME,
       password: process.env.RDS_PASSWORD,
       ssl: {
         rejectUnauthorized: false,
       },
     });
+
+    await dataSource
+      .initialize()
+      .then(() => console.info("Using AWS DataSource connection"));
+
+    return dataSource;
   }
 
   throw new Error("Unknown environment");
