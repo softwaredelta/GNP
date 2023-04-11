352ba47226268ba3c4a984a9cbaebd608a0ffbe7
JordanaBM <A01707434@itesm.mx>
2023-04-10 23:19:38 -0600

    [FRONTEND] Footer, Navbar and Wrapper Component
    Co-authored-by: Alejandro Martínez Luna <A01276785@tec.mx>
    Co-authored-by: karen-baltazar <karen-baltazar@users.noreply.github.com>
    Co-authored-by: JordanaBM <JordanaBM@users.noreply.github.com>
    Co-authored-by: Monica Ayala Marrero <a01707439@itesm.mx>

---
+++ ram-front/src/components/includes/Footer/Foot.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Footer } from "flowbite-react";
+
+import wavesRAM from "../../../assets/imgs/wavesRAM.png";
+
+type Props = {};
+
+function Foot({}: Props) {
+  return (
+    <Footer container={false}>
+      {/* <div className='h-20 w-full'> */}
+      <img className="h-60 w-full" src={wavesRAM} alt="Footer de la página " />
+    </Footer>
+  );
+}
+
+export default Foot;
---
+++ ram-front/src/containers/Wrapper.tsx
@@ -0,0 +1,18 @@
+import Foot from "../components/includes/Footer/Foot";
+import NavBar from "../components/includes/NavBar/NavBar";
+
+type Props = {};
+
+const Wrapper = ({ children }: any): JSX.Element => {
+  return (
+    <main className="h-screen overflow-y-auto">
+      <NavBar />
+      <div className="container mx-auto grid">
+        <h1 className="text-4xl">Hola amigos</h1>
+      </div>
+      <Foot />
+    </main>
+  );
+};
+
+export default Wrapper;
