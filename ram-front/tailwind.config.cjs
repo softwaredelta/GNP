fabc5626f9003c9bcb277b4fe6162de105bdeb1f
JordanaBM <A01707434@itesm.mx>
2023-04-10 23:18:20 -0600

    [FRONTEND] Added custom color classes
    Co-authored-by: Alejandro Martínez Luna <A01276785@tec.mx>
    Co-authored-by: karen-baltazar <karen-baltazar@users.noreply.github.com>
    Co-authored-by: JordanaBM <JordanaBM@users.noreply.github.com>
    Co-authored-by: Monica Ayala Marrero <a01707439@itesm.mx>

--- ram-front/tailwind.config.cjs
+++ ram-front/tailwind.config.cjs
@@ -1,8 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}", "./src/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/*.{js,jsx,ts,tsx}",
+    "./node_modules/flowbite/**/*.js",
+    "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
+  ],
+
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        GNP_Azul_Light: "#579dc3",
+        GNP_Azul: "#012e71",
+        GNP_Azul_Darker: "#012356",
+
+        GNP_Naranja_Light: "#be7743",
+        GNP_Naranja: "#fb6f24",
+        GNP__Naranja_Darker: "#e15104",
+
+        GNP_Crema: "#e6dcd1",
+        GNP_Cafe: "#7e4e3a",
+        GNP_Gris_Dark: "#878584",
+        GNP_Gris_Light: "#bcbcbc",
+
+        GNP_BurlyWood: "#ba8c70",
+        GNP_NavajoWhite: "#d0a97e",
+        GNP_White: "#e9e9e9",
+      },
+    },
   },
-  plugins: [],
+  plugins: [require("flowbite/plugin")],
 };
