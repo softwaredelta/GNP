6727f3ed45179a3be55caaab09fbb04af6b58846
Naiztu <a01707404@tec.mx>
2023-04-13 16:21:43 -0600

    [FRONT] Added dynamic values for CardInfoAssurance
    Co-authored-by: JordanaBM <JordanaBM@users.noreply.github.com>
    Co-authored-by: Alejandro Martínez Luna <A01276785@tec.mx>

--- ram-front/src/components/generics/CardInfoAssurence.tsx
+++ ram-front/src/components/generics/CardInfoAssurence.tsx
@@ -1,32 +1,37 @@
 // (c) Delta Software 2023, rights reserved.
 
-import React from "react";
-import { BsHouses } from "react-icons/bs";
 import { FaWallet } from "react-icons/fa";
+import { NumericFormat } from "react-number-format";
 
 export interface ICardInfoAssuranceProps {
-  title?: string;
-  total?: number;
-  icon?: React.ReactNode;
+  typeAssurance: string;
+  total: number;
+  color?: string;
 }
 
 function CardInfoAssurence({
-  title,
+  typeAssurance,
   total,
-  icon,
+  color,
 }: ICardInfoAssuranceProps): JSX.Element {
   return (
     <div className="mt-3 h-full w-full items-center justify-center rounded rounded-xl bg-gnp-white">
-      <h1 className=" text-center text-2xl font-bold text-black">
-        Tipo de Seguro
+      <h1 className=" text-center text-2xl font-bold text-black w-10/12 mx-auto">
+        {typeAssurance}
       </h1>
-      <h2 className="text-center mt-2 font-bold text-md text-gnp-gray-black">
+      <h2 className="text-center mt-2 font-bold text-md text-gnp-gray-black ">
         Monto Total Vendido:
       </h2>
       <div className="flex items-center justify-center mt-3">
-        <FaWallet size={25} className="fill-gnp-primary-orange mt-2 mr-4" />
-        <h3 className=" text-center mt-2 font-bold text-lg text-gnp-primary-orange">
-          $1,000,000 <strong>MXN</strong>
+        <FaWallet size={25} className={`fill-gnp-${color} mt-2 mr-4`} />
+        <h3 className={`text-center mt-2 font-bold text-lg text-gnp-${color}`}>
+          <NumericFormat
+            value={total}
+            displayType={"text"}
+            thousandSeparator={true}
+            prefix={"$"}
+          />{" "}
+          <strong>MXN</strong>
         </h3>
       </div>
     </div>
