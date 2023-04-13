60ca9ed021e0fb2fb453fb9db0e99b944772cafd
Naiztu <a01707404@tec.mx>
2023-04-13 15:32:14 -0600

    [FRONT] Minor changes

--- ram-front/src/components/includes/Footer/Foot.tsx
+++ ram-front/src/components/includes/Footer/Foot.tsx
@@ -1,16 +1,9 @@
-// (c) Delta Software 2023, rights reserved.
-
-import React from "react";
 import { Footer } from "flowbite-react";
-
 import wavesRAM from "../../../assets/imgs/wavesRAM.png";
 
-type Props = {};
-
-function Foot({}: Props) {
+function Foot() {
   return (
     <Footer container={false}>
-      {/* <div className='h-20 w-full'> */}
       <img className="h-60 w-full" src={wavesRAM} alt="Footer de la página " />
     </Footer>
   );
