8de597750b62d8cb1afdd717834f06d185dc034d
Naiztu <a01707404@tec.mx>
2023-04-12 23:24:28 -0600

    [FRONT] Added custom card components

---
+++ ram-front/src/components/generics/CardInfoTopFive.tsx
@@ -0,0 +1,13 @@
+export default function CardInfoTopFive() {
+  return (
+    <div className="mt-3 h-full w-full items-center justify-center rounded rounded-xl bg-gnp-white">
+      <h1 className=" text-center text-2xl font-bold text-black">
+        Tipo de Seguro
+      </h1>
+      <h2 className="text-center mt-2 font-bold text-xs text-gnp-gray-black">
+        Top 5 vendedores por monto acumulado:
+      </h2>
+      {/* <br>1.- Judy Marks - $ 1,000,000.00</br> */}
+    </div>
+  );
+}
