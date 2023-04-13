86dbf538220109c38ecc873ebce2815e38090675
Alejandro Martínez Luna <A01276785@tec.mx>
2023-04-12 21:17:36 -0600

    [FRONTEND] Modified Wrapper and Layout component
    Co-authored-by: Naiztu <Naiztu@users.noreply.github.com>

--- ram-front/src/containers/Wrapper.tsx
+++ ram-front/src/containers/Wrapper.tsx
@@ -1,3 +1,4 @@
+import { Children } from "react";
 import Foot from "../components/includes/Footer/Foot";
 import NavBar from "../components/includes/NavBar/NavBar";
 
@@ -7,8 +8,8 @@ const Wrapper = ({ children }: any): JSX.Element => {
   return (
     <main className="h-screen overflow-y-auto">
       <NavBar />
-      <div className="container mx-auto grid">
-        <h1 className="text-4xl">Hola amigos</h1>
+      <div className="container mx-auto grid text-gnp">
+        <h1 className="text-4xl text-gnp-primary-blue">Hola Amigos</h1>
       </div>
       <Foot />
     </main>
