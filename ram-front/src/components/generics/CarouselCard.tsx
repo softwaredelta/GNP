c3050624c3a304753aafdf6598e6a0b973161b0a
Naiztu <a01707404@tec.mx>
2023-04-13 18:14:28 -0600

    [FRONT] Minor changes
    Co-authored-by: JordanaBM <JordanaBM@users.noreply.github.com>
    Co-authored-by: Alejandro Martínez Luna <A01276785@tec.mx>

--- ram-front/src/components/generics/CarouselCard.tsx
+++ ram-front/src/components/generics/CarouselCard.tsx
@@ -1,13 +1,10 @@
 // (c) Delta Software 2023, rights reserved.
 
-import React from "react";
-import { Carousel, Button } from "flowbite-react";
+import { Carousel } from "flowbite-react";
 import CardInfoTopFive from "./CardInfoTopFive";
 import Card from "./Card";
 
 import { IoPawSharp } from "react-icons/io5";
-
-
 
 const CarouselCard = () => {
   return (
@@ -18,21 +15,60 @@ const CarouselCard = () => {
           color="primary-blue"
           icon={<IoPawSharp color="white" size={30} />}
         >
-          <CardInfoTopFive />
+          <CardInfoTopFive
+            typeAssurance="Seguro de mascotas"
+            color="primary-blue"
+            top={[
+              {
+                name: "Alfonso Cuarón",
+                amount: 200000,
+              },
+              {
+                name: "Alfonso Cuarón",
+                amount: 200000,
+              },
+            ]}
+          />
         </Card>
         <Card
           image="https://imgs.search.brave.com/muR0HGm76B6gbdiwyaBULBApAqvAdlWv2aHFAsQYVUw/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly93YWxs/cGFwZXJhY2Nlc3Mu/Y29tL2Z1bGwvNTA4/OTUyOC5qcGc"
           color="primary-orange"
           icon={<IoPawSharp color="white" size={30} />}
         >
-          <CardInfoTopFive />
+          <CardInfoTopFive
+            typeAssurance="Seguro de mascotas"
+            color="primary-blue"
+            top={[
+              {
+                name: "Alfonso Cuarón",
+                amount: 200000,
+              },
+              {
+                name: "Alfonso Cuarón",
+                amount: 200000,
+              },
+            ]}
+          />
         </Card>
         <Card
           image="https://imgs.search.brave.com/muR0HGm76B6gbdiwyaBULBApAqvAdlWv2aHFAsQYVUw/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly93YWxs/cGFwZXJhY2Nlc3Mu/Y29tL2Z1bGwvNTA4/OTUyOC5qcGc"
           color="light-orange"
           icon={<IoPawSharp color="white" size={30} />}
         >
-          <CardInfoTopFive />
+          <CardInfoTopFive
+            typeAssurance="Seguro de mascotas"
+            color="primary-blue"
+            top={[
+              {
+                name: "Alfonso Cuarón",
+                amount: 200000,
+              },
+              {
+                name: "Alfonso Cuarón",
+                amount: 200000,
+              },
+            ]}
+          />
         </Card>
       </Carousel>
     </div>
--- ram-front/src/containers/Wrapper.tsx
+++ ram-front/src/containers/Wrapper.tsx
@@ -1,11 +1,13 @@
 // (c) Delta Software 2023, rights reserved.
 
-import { Children } from "react";
 import Foot from "../components/includes/Footer/Foot";
 import NavBar from "../components/includes/NavBar/NavBar";
 
+export interface IWrapperProps {
+  children: JSX.Element;
+}
 
-const Wrapper = ({ children }: any): JSX.Element => {
+const Wrapper = ({ children }: IWrapperProps): JSX.Element => {
   return (
     <main className="h-screen overflow-y-auto">
       <NavBar />
