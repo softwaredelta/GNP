3d776b033a429f94e89976f4f7ccbd820911181f
Kenny Vercaemer <vercaemereduard@gmail.com>
2023-04-14 14:03:06 -0600

    [Test] Fix jest to test React components

--- ram-front/babel.config.js
+++ ram-front/babel.config.js
@@ -3,5 +3,12 @@ module.exports = {
     ["@babel/preset-env", { targets: { esmodules: true } }],
     ["@babel/preset-react", { runtime: "automatic" }],
     "@babel/preset-typescript",
+    [
+      "babel-preset-vite",
+      {
+        env: true, // defaults to true
+        glob: false, // defaults to true
+      },
+    ],
   ],
 };
---
+++ ram-front/src/lib/api/api-time.ts
@@ -0,0 +1,43 @@
+// (c) Delta Software 2023, rights reserved.
+
+import { atom, selector } from "recoil";
+import { apiBase$, isTest$ } from "./api-base";
+
+const update = atom({
+  key: "apiTimeUpdate",
+  default: 0,
+});
+
+export interface ApiTime {
+  now: string;
+  update: () => void;
+}
+
+export const apiTime$ = selector<ApiTime>({
+  key: "apiTimeSelector",
+  get: async ({ get, getCallback }) => {
+    const apiBase = get(apiBase$);
+    const isTest = get(isTest$);
+    get(update);
+
+    const updateTime = getCallback(({ set }) => () => {
+      set(update, (v) => v + 1);
+    });
+
+    if (isTest) {
+      return {
+        now: new Date().toISOString(),
+        update: updateTime,
+      };
+    }
+
+    const { now } = (await fetch(`${apiBase}/time`).then((r) =>
+      r.json(),
+    )) as any;
+
+    return {
+      now,
+      update: updateTime,
+    };
+  },
+});
