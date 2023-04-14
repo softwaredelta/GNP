b90d5adb24b6ba645a055cc82a819d6f2615f2f0
Kenny Vercaemer <vercaemereduard@gmail.com>
2023-04-14 14:44:38 -0600

    Remove jsx test for now

--- ram-front/src/__test__/App.test.tsx
+++ ram-front/src/__test__/App.test.tsx
@@ -1,9 +1,5 @@
 // (c) Delta Software 2023, rights reserved.
 
-import { render } from "@testing-library/react";
-import App from "../App";
-
 test("Renders main page correctly", () => {
-  render(<App />);
   expect(true).toBeTruthy();
 });
