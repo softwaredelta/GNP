1efbddb690ba2dd1b69bd0c6d04d1b308fba475f
Kenny Vercaemer <vercaemereduard@gmail.com>
2023-04-14 12:10:30 -0600

    [Test] Add example http test

---
+++ ram-back/src/__tests__/http.test.ts
@@ -0,0 +1,16 @@
+// (c) Delta Software 2023, rights reserved.
+
+import request from "supertest";
+import { app } from "../app";
+
+describe("HTTP", () => {
+  it("Should have an http server", () => {
+    return request(app)
+      .get("/health")
+      .expect("Content-Type", /text\/html/)
+      .expect(200)
+      .then((response) => {
+        expect(response.text).toBe("OK");
+      });
+  });
+});
--- ram-back/src/index.ts
+++ ram-back/src/index.ts
@@ -1,37 +1,8 @@
 // (c) Delta Software 2023, rights reserved.
 
-import "reflect-metadata";
-import path from "path";
-import express, { Application } from "express";
-import cors from "cors";
-import bodyParser from "body-parser";
-import * as dotenv from "dotenv";
-import { router } from "./routes";
-import { getDataSource } from "./arch/db-client";
+import { start } from "./app";
 
-// for local development, we use the env file
-if (!process.env.NODE_ENV) {
-  console.info("Using local .env.development file");
-  dotenv.config({
-    path: path.resolve(__dirname, "../../.env.development"),
-  });
-}
-
-const app: Application = express();
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
-
-app.use(process.env.API_PREFIX || "/", router);
-
-app.use((req, res) => {
-  res.redirect(process.env.APP_URL || "/");
-});
-
-const port = process.env.API_PORT || 8080;
-app.listen(port, async () => {
-  // make sure db connection is initialized
-  const ds = await getDataSource();
-  console.log("Server listening on port " + port);
+start().catch((err) => {
+  console.error(err);
+  process.exit(1);
 });
