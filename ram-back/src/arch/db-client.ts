cea9748ab218a12f756f8da0336f5314f9219b6c
Alejandro Martínez Luna <A01276785@tec.mx>
2023-04-16 13:40:21 -0600

    [Auth] Implement auth module

--- ram-back/src/arch/db-client.ts
+++ ram-back/src/arch/db-client.ts
@@ -35,6 +35,12 @@ export async function getDataSource(): Promise<DataSource> {
     );
     await dataSource.synchronize(true);
 
+    // On local development we want to initialize seeds
+    if (!process.env.NODE_ENV) {
+      console.warn("Loading seeds...");
+      await loadSeeds();
+    }
+
     return dataSource;
   }
 
