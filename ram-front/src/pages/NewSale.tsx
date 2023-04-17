32c0d13ebd4cd74dc7516f65abef88f4b8051133
Monica <a01707439@itesm.mx>
2023-04-17 14:40:45 -0600

    Reformated with prettier

--- ram-front/src/pages/NewSale.tsx
+++ ram-front/src/pages/NewSale.tsx
@@ -2,8 +2,7 @@ import React, { useState, ChangeEvent } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import moneyGrowth from "../assets/imgs/moneyGrowth.png";
-import { TbSend } from "react-icons/tb"
-
+import { TbSend } from "react-icons/tb";
 
 export function NewSale() {
   const [selectedDate, setSelectedDate] = useState<Date | null>(null);
@@ -28,97 +27,102 @@ export function NewSale() {
         </div>
         <div className="w-3/4 h-full">
           <div className="bg-orange-500 p-4 flex justify-center items-center">
-            <h1 className="text-3xl my-2 font-bold text-white">Agregar venta</h1>
+            <h1 className="text-3xl my-2 font-bold text-white">
+              Agregar venta
+            </h1>
           </div>
           <div className="p-4">
-          <form onSubmit={handleSubmit} className="flex flex-wrap">
-            <div className="w-1/2 p-2">
-              <label className="block text-gray-700 text-sm font-bold mb-2">
-                Poliza
-              </label>
-              <input
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                type="text"
-                placeholder="Ingrese el número de poliza"
-                value={poliza}
-                onChange={(e: ChangeEvent<HTMLInputElement>) =>
-                  setPoliza(e.target.value)
-                }
-              />
-            </div>
-            <div className="w-1/2 p-2">
-              <label className="block text-gray-700 text-sm font-bold mb-2">
-                Fecha
-              </label>
-              <DatePicker
-                selected={selectedDate}
-                onChange={(date: Date | null) => setSelectedDate(date)}
-                dateFormat="dd/MM/yyyy"
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              />
-            </div>
-            <div className="w-1/2 p-2">
-              <label className="block text-gray-700 text-sm font-bold mb-2">
-                Monto
-              </label>
-              <input
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                type="number"
-                placeholder="Ingrese el monto de la venta"
-                value={monto}
-                onChange={(e: ChangeEvent<HTMLInputElement>) =>
-                  setMonto(e.target.value)
-                }
-              />
-            </div>
-            <div className="w-1/2 p-2">
-              <label className="block text-gray-700 text-sm font-bold mb-2">
-               Nombre del Cliente
-              </label>
-              <input
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                type="text"
-                placeholder="Ingrese el nombre del cliente"
-                value={client}
-                onChange={(e: ChangeEvent<HTMLInputElement>) =>
-                  setClient(e.target.value)
-                }
-              />
-            </div>
-            <div className="w-1/2 p-2">
-              <label className="block text-gray-700 text-sm font-bold mb-2">
-               Adjuntar evidencia
-              </label>
-              <input
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                type="text"
-                placeholder="Ingrese el nombre del cliente"
-                value={client}
-                onChange={(e: ChangeEvent<HTMLInputElement>) =>
-                  setClient(e.target.value)
-                }
-              />
-            </div>
-            <div className="w-1/2 p-2">
-              <label className="block text-gray-700 text-sm font-bold mb-2">
-               Tipo de seguro
-              </label>
-              <input
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                type="text"
-                placeholder="Ingrese el nombre del cliente"
-                value={client}
-                onChange={(e: ChangeEvent<HTMLInputElement>) =>
-                  setClient(e.target.value)
-                }
-              />
-            </div>
-<div className="w-full p-2 flex justify-center items-center">
-              <button className="bg-gnp-primary-blue hover:bg-gnp-primary-blue text-white font-bold flex justify-center items-center py-2 mt-4 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
-                Enviar <TbSend size={20} className="ml-2"/>
-              </button>
-            </div>
-          </form>
+            <form onSubmit={handleSubmit} className="flex flex-wrap">
+              <div className="w-1/2 p-2">
+                <label className="block text-gray-700 text-sm font-bold mb-2">
+                  Poliza
+                </label>
+                <input
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  type="text"
+                  placeholder="Ingrese el número de poliza"
+                  value={poliza}
+                  onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                    setPoliza(e.target.value)
+                  }
+                />
+              </div>
+              <div className="w-1/2 p-2">
+                <label className="block text-gray-700 text-sm font-bold mb-2">
+                  Fecha
+                </label>
+                <DatePicker
+                  selected={selectedDate}
+                  onChange={(date: Date | null) => setSelectedDate(date)}
+                  dateFormat="dd/MM/yyyy"
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                />
+              </div>
+              <div className="w-1/2 p-2">
+                <label className="block text-gray-700 text-sm font-bold mb-2">
+                  Monto
+                </label>
+                <input
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  type="number"
+                  placeholder="Ingrese el monto de la venta"
+                  value={monto}
+                  onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                    setMonto(e.target.value)
+                  }
+                />
+              </div>
+              <div className="w-1/2 p-2">
+                <label className="block text-gray-700 text-sm font-bold mb-2">
+                  Nombre del Cliente
+                </label>
+                <input
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  type="text"
+                  placeholder="Ingrese el nombre del cliente"
+                  value={client}
+                  onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                    setClient(e.target.value)
+                  }
+                />
+              </div>
+              <div className="w-1/2 p-2">
+                <label className="block text-gray-700 text-sm font-bold mb-2">
+                  Adjuntar evidencia
+                </label>
+                <input
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  type="text"
+                  placeholder="Ingrese el nombre del cliente"
+                  value={client}
+                  onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                    setClient(e.target.value)
+                  }
+                />
+              </div>
+              <div className="w-1/2 p-2">
+                <label className="block text-gray-700 text-sm font-bold mb-2">
+                  Tipo de seguro
+                </label>
+                <input
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  type="text"
+                  placeholder="Ingrese el nombre del cliente"
+                  value={client}
+                  onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                    setClient(e.target.value)
+                  }
+                />
+              </div>
+              <div className="w-full p-2 flex justify-center items-center">
+                <button
+                  className="bg-gnp-primary-blue hover:bg-gnp-primary-blue text-white font-bold flex justify-center items-center py-2 mt-4 px-4 rounded focus:outline-none focus:shadow-outline"
+                  type="submit"
+                >
+                  Enviar <TbSend size={20} className="ml-2" />
+                </button>
+              </div>
+            </form>
           </div>
         </div>
       </div>
