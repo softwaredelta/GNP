efd499a45775ed1af8fb415d343286a336d8c8b7
Monica <a01707439@itesm.mx>
2023-04-18 16:06:01 -0600

    [FRONT] Minor changes

--- ram-front/src/pages/SalesHistory.tsx
+++ ram-front/src/pages/SalesHistory.tsx
@@ -1,16 +1,15 @@
 // (c) Delta Software 2023, rights reserved.
 
-import React from "react";
+import React, { useState } from "react";
 import { SalesTable } from "../components/SalesHistory/SalesTable";
 import { SalesFilters } from "../components/SalesHistory/SalesFilters";
 import Wrapper from "../containers/Wrapper";
 import { Pagination } from "flowbite-react";
-import {useState} from "react";
 
 export const SalesHistory = () => {
   const [indexStart, setIndexStart] = useState(0);
   const [indexEnd, setIndexEnd] = useState(5);
-  function onPageChange(){
+  function onPageChange() {
     // if (newPage < currentPage){
     //   setIndexStart(indexStart - 5);
     //   setIndexEnd(indexEnd - 5);
@@ -24,14 +23,13 @@ export const SalesHistory = () => {
         <SalesFilters />
         <SalesTable indexStart={indexStart} indexEnd={indexEnd} />
         <div className="flex items-center justify-center text-center">
-  <Pagination
-  
-    currentPage={1}
-    layout="table"
-    onPageChange={onPageChange}
-    totalPages={1000}
-  />
-</div>
+          <Pagination
+            currentPage={1}
+            layout="table"
+            onPageChange={onPageChange}
+            totalPages={1000}
+          />
+        </div>
       </div>
     </Wrapper>
   );
