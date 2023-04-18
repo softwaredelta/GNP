576770e1ede732e3da42e1b98ad56f0fda8b3b13
Monica <a01707439@itesm.mx>
2023-04-18 16:01:12 -0600

    [FRONT] Started using component library

--- ram-front/src/pages/SalesHistory.tsx
+++ ram-front/src/pages/SalesHistory.tsx
@@ -3,12 +3,36 @@
 import React from "react";
 import { SalesTable } from "../components/SalesHistory/SalesTable";
 import { SalesFilters } from "../components/SalesHistory/SalesFilters";
+import Wrapper from "../containers/Wrapper";
+import { Pagination } from "flowbite-react";
+import {useState} from "react";
 
 export const SalesHistory = () => {
+  const [indexStart, setIndexStart] = useState(0);
+  const [indexEnd, setIndexEnd] = useState(5);
+  function onPageChange(){
+    // if (newPage < currentPage){
+    //   setIndexStart(indexStart - 5);
+    //   setIndexEnd(indexEnd - 5);
+    // }
+    setIndexEnd(indexEnd + 5);
+    setIndexStart(indexStart + 5);
+  }
   return (
-    <div className="flex flex-col h-screen justify-center items-center">
-      <SalesFilters />
-      <SalesTable />
-    </div>
+    <Wrapper>
+      <div className="flex flex-col mt-8 justify-center items-center">
+        <SalesFilters />
+        <SalesTable indexStart={indexStart} indexEnd={indexEnd} />
+        <div className="flex items-center justify-center text-center">
+  <Pagination
+  
+    currentPage={1}
+    layout="table"
+    onPageChange={onPageChange}
+    totalPages={1000}
+  />
+</div>
+      </div>
+    </Wrapper>
   );
 };
