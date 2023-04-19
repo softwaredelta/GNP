2513ee2110dc9e65b1840c177cf97b23b510dd96
Monica <a01707439@itesm.mx>
2023-04-18 19:02:35 -0600

    [FRONT] Implemented component test for New Sale

--- ram-front/src/__test__/newsale.test.tsx
+++ ram-front/src/__test__/newsale.test.tsx
@@ -56,9 +56,7 @@ describe("NewSale", () => {
 
   it("updates client name input on change", () => {
     render(<NewSale />);
-    const clientInput: HTMLInputElement = screen.getByPlaceholderText(
-      "Ingrese el nombre del cliente",
-    );
+    const clientInput: HTMLInputElement = screen.getByTestId("client-name");
     fireEvent.change(clientInput, { target: { value: "John Doe" } });
     expect(clientInput.value).toBe("John Doe");
   });
