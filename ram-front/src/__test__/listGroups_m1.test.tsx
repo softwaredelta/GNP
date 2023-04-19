37544987b7c40b12d48896fc0b613fb92c427929
Naiztu <a01707404@tec.mx>
2023-04-18 18:31:52 -0600

    [FRONT] Added more test for component List Group

--- ram-front/src/__test__/listGroups_m1.test.tsx
+++ ram-front/src/__test__/listGroups_m1.test.tsx
@@ -1,4 +1,5 @@
-import "@testing-library/jest-dom";
+// (c) Delta Software 2023, rights reserved.
+import "@testing-library/react";
 import { render, screen } from "@testing-library/react";
 import ListGroups from "../components/groups/ListGroup";
 
@@ -11,16 +12,50 @@ describe("ListGroups", () => {
             id: "fdsf-fdsfsf",
             name: "Group 1",
             progress: 50,
+            image: "https://picsum.photos/200",
           },
           {
             id: "fdsf-fdsfsf-fddfs",
             name: "Group 2",
             progress: 80,
+            image: "https://picsum.photos/200",
           },
         ]}
       />,
     );
     const groups = screen.getAllByRole("group");
     expect(groups).toHaveLength(2);
+  });
+
+  it("renders a message when there are no groups", () => {
+    render(<ListGroups groups={[]} />);
+    const message = screen.getByText("No hay grupos");
+    expect(message).toBeInTheDocument();
+  });
+
+  it("renders a group", () => {
+    render(
+      <ListGroups
+        groups={[
+          {
+            id: "fdsf-fdsfsf",
+            name: "Group 1",
+            progress: 50,
+            image: "https://picsum.photos/200",
+          },
+        ]}
+      />,
+    );
+    const group = screen.getByRole("group");
+    expect(group).toBeInTheDocument();
+
+    const name = screen.getByText("Group 1");
+    expect(name).toBeInTheDocument();
+
+    const progress = screen.getByText("50%");
+    expect(progress).toBeInTheDocument();
+
+    const image = screen.getByRole("img");
+    expect(image).toBeInTheDocument();
   });
 });
