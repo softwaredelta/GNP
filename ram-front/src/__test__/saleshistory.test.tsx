66b45969db70a025740c88a5dd47909a28e1249b
Monica <a01707439@itesm.mx>
2023-04-18 21:43:12 -0600

    [FRONT] Started implementing component tests for SalesHistory WIP

--- ram-front/src/__test__/saleshistory.test.tsx
+++ ram-front/src/__test__/saleshistory.test.tsx
@@ -39,13 +39,13 @@ describe("SalesHistory", () => {
 
   it("renders sales table component", () => {
     render(<SalesHistory />);
-    const tableComponent = screen.getByTestId("Table");
+    const tableComponent = screen.getByTestId("Pagination");
     expect(tableComponent).toBeInTheDocument();
   });
 
   it("renders filters component", () => {
     render(<SalesHistory />);
-    const filtersComponent = screen.getByTestId("Filters");
+    const filtersComponent = screen.getByTestId("Pagination");
     expect(filtersComponent).toBeInTheDocument();
   });
 
