98a2347696e7361d512d753d75cf517a0d95e087
Naiztu <a01707404@tec.mx>
2023-04-19 00:51:02 -0600

    [FRONT] Added Skeleton Components
    Co-authored-by: Alejandro Martínez Luna <A01276785@tec.mx>

---
+++ ram-front/src/components/generics/skeleton/SkeletonDiv.tsx
@@ -0,0 +1,8 @@
+export default function SkeletonDiv(): JSX.Element {
+  return (
+    <div role="status" className="animate-pulse h-full w-full border">
+      <div className="bg-gray-300 w-full h-full dark:bg-gray-700  mb-2.5 mx-auto"></div>
+      <span className="sr-only">Loading...</span>
+    </div>
+  );
+}
