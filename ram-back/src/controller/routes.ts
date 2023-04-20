ccc3aea9b037819be5f069b738cffc8f0700b971
Monica <a01707439@itesm.mx>
2023-04-20 14:27:07 -0600

    [FRONT] Rebase issues corrected again

--- ram-back/src/controller/routes.ts
+++ ram-back/src/controller/routes.ts
@@ -4,9 +4,11 @@ import { Router } from "express";
 import { authRouter } from "./user";
 import { infraRouter } from "./infra";
 import { groupsRouter } from "./group";
+import { salesRouter } from "./sale";
 
 export const router = Router();
 
 router.use("/user", authRouter);
 router.use("/infra", infraRouter);
 router.use("/groups", groupsRouter);
+router.use("/sales", salesRouter);
