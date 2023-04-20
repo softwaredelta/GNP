f394c63241abdbe54841fc94ceea1e70a58b8672
Ian García <ian.garcia@nibbio.co>
2023-04-19 20:32:30 -0600

    [BACK] Implemented user delivery router.
    Co-authored-by: Alejandro Martínez Luna <A01276785@tec.mx>

--- ram-back/src/controller/routes.ts
+++ ram-back/src/controller/routes.ts
@@ -4,9 +4,11 @@ import { Router } from "express";
 import { authRouter } from "./user";
 import { infraRouter } from "./infra";
 import { groupRouter } from "./group";
+import {userDeliveryRouter} from "./user-delivery";
 
 export const router = Router();
 
 router.use("/user", authRouter);
 router.use("/infra", infraRouter);
 router.use("/group", groupRouter);
+router.use("/user-delivery", userDeliveryRouter);
