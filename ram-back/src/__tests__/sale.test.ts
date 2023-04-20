d18de5ff644a604d5cd3dfe96b82cdd93f3dbcaa
Monica <a01707439@itesm.mx>
2023-04-20 15:34:30 -0600

    [BACK] Add createSale test

--- ram-back/src/__tests__/sale.test.ts
+++ ram-back/src/__tests__/sale.test.ts
@@ -4,17 +4,22 @@
 // import { app } from "../controller";
 import { createSale } from "../app/sale";
 import { getDataSource } from "../arch/db-client";
+import { createAssuranceType } from "../app/assuranceType";
 
 describe("sale", () => {
   beforeEach(async () => {
     const ds = await getDataSource();
     await ds.synchronize(true);
+    await createAssuranceType({
+      name: "test-assurance-type-1",
+      description: "test-assurance-type-1-description",
+      id: "test-at-1",
+    });
   });
 
   describe("creation function works", () => {
     it("creates new sale", async () => {
       const { sale, error } = await createSale({
-        id: "123456",
         policyNumber: "123456",
         assuranceType: {
           name: "test-assurance-type-1",
@@ -26,12 +31,11 @@ describe("sale", () => {
         clientName: "Juan Perez",
       });
 
-      expect(sale).toBeDefined();
-      expect(error).toBeDefined();
-      //   expect(sale).toHaveProperty("id");
-      //   expect(sale).toHaveProperty("policyNumber", "123456");
-      //   expect(sale).toHaveProperty("clientName", "Juan Perez");
-      //   expect(sale).toHaveProperty("amountInCents", "123456");
+      expect(error).toBeUndefined();
+      expect(sale).toHaveProperty("id");
+      expect(sale).toHaveProperty("policyNumber", "123456");
+      expect(sale).toHaveProperty("clientName", "Juan Perez");
+      expect(sale).toHaveProperty("amountInCents", "123456");
     });
   });
 });
