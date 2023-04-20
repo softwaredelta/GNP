bd51f9fdcd0895e280a26696c768a01534a1f831
Monica <a01707439@itesm.mx>
2023-04-20 15:19:26 -0600

    [BACK] Fixed conflicts

--- ram-back/src/controller/sale.ts
+++ ram-back/src/controller/sale.ts
@@ -7,30 +7,19 @@ import * as j from "joi";
 
 const userParameters = j.object({
   policyNumber: j.string().required(),
-<<<<<<< HEAD
   assuranceType: j.object().required(),
-=======
-  assuranceType: j.string().required(),
->>>>>>> 23639a06391f34e99408a102d8ab1c7dc352d37e
   sellDate: j.string().required(),
   amountInCents: j.string().required(),
   clientName: j.string().required(),
 });
 
-<<<<<<< HEAD
 const saleParametersMiddleware: RequestHandler = (req, res, next) => {
-=======
-const saleParametersMiddleware: RequestHandler = (req, res) => {
->>>>>>> 23639a06391f34e99408a102d8ab1c7dc352d37e
   const { error } = userParameters.validate(req.body);
   if (error) {
     res.status(400).json({ message: "BAD_DATA", reason: error });
     return;
   }
-<<<<<<< HEAD
   next();
-=======
->>>>>>> 23639a06391f34e99408a102d8ab1c7dc352d37e
 };
 
 salesRouter.post("/create", saleParametersMiddleware, async (req, res) => {
