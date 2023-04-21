67f0c4aba2d20c1a2e93edae563351d1ae555cc8
Alejandro Martínez Luna <A01276785@tec.mx>
2023-04-21 05:20:33 -0600

    [FRONT] Added preview when uploading file

--- ram-front/src/components/Delivery.tsx
+++ ram-front/src/components/Delivery.tsx
@@ -1,18 +1,32 @@
 // (c) Delta Software 2023, rights reserved.
 
-import Wrapper from "../containers/Wrapper";
-import DeliveryCard from "./generics/cards/DeliveryCard";
-import useModal from "../hooks/useModal";
-import { useRef, useState } from "react";
-import Modal from "./generics/Modal";
-import DropZone from "./generics/DropZone";
 import axios from "axios";
+import { useEffect, useRef, useState } from "react";
+import { AiOutlineFileProtect } from "react-icons/ai";
+import Wrapper from "../containers/Wrapper";
+import useModal from "../hooks/useModal";
 import { Button } from "./button";
+import DropZone from "./generics/DropZone";
+import Modal from "./generics/Modal";
+import DeliveryCard from "./generics/cards/DeliveryCard";
 
 function Delivery() {
+  // Get data using axios from http://localhost:8080/user-delivery
+  const [delivery, setDelivery] = useState<any>([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:8080/user-delivery").then((response) => {
+      setDelivery(response.data);
+    });
+  }, []);
+
   const [id, setId] = useState<string>("");
   const { isOpen, toggleModal } = useModal(false);
   const modalFileInput = useRef<HTMLInputElement>(null);
+
+  const [uploadFileURL, setUploadFileURL] = useState<string | null>("");
+
+  const [fileName, setFileName] = useState<string | null>(null);
 
   const handleModalOpen = (deliveryCardID: string): void => {
     setId(deliveryCardID);
@@ -30,6 +44,9 @@ function Delivery() {
         (async (): Promise<void> => {
           const response = await axios.post("", formData);
           console.debug(response);
+
+          setUploadFileURL(response.data); //Se guarda la url del archivo en el estado
+          setFileName(response.headers["filename"]); //Se guarda el nombre del archivo en el estado
         })();
       } catch (error) {
         console.error(error);
@@ -37,44 +54,72 @@ function Delivery() {
     }
   };
 
+  const openFileInNewTab = (): void => {
+    if (uploadFileURL) {
+      window.open(uploadFileURL);
+    }
+  };
+
   return (
-    <>
-      <Wrapper>
-        <>
-          <div className="w-full min-h-[50vh] grid md:grid-cols-3 place-items-center gap-10 py-20">
-            <div className="md:col-span-3 space-y-10 ">
+    <Wrapper>
+      <>
+        <div className="w-full min-h-[50vh] grid md:grid-cols-3 place-items-center gap-10 py-20">
+          <div className="md:col-span-3 space-y-10 ">
+            {delivery.map((delivery: any) => (
               <DeliveryCard
                 color="blue"
                 nameDelivery="Nombre de la entrega"
-                status="Sin enviar"
+                status={delivery.status}
                 onFileSubmit={handleModalOpen}
-                image="https://i.blogs.es/799a0e/ydray-mew_27_articuno_45l_hyperx_environment_front/1366_2000.jpeg"
+                image={delivery.fileUrl}
               />
-              <DeliveryCard
-                color="orange"
-                nameDelivery="Nombre de la entrega segunda"
-                status="Rechazado"
-                onFileSubmit={handleModalOpen}
-                image="https://i.blogs.es/799a0e/ydray-mew_27_articuno_45l_hyperx_environment_front/1366_2000.jpeg"
-              />
-            </div>
+            ))}
           </div>
-          {isOpen && (
-            <Modal closeModal={toggleModal}>
-              <div className="flex flex-col space-y-5">
-                <h3 className="text-4xl text-gnp-blue-600 font-bold my-4">
-                  Sube tu evidencia
-                </h3>
+        </div>
+        {isOpen && (
+          <Modal closeModal={toggleModal}>
+            <div className="flex flex-col space-y-5">
+              <h3 className="text-4xl text-gnp-blue-600 font-bold my-4">
+                Sube tu evidencia
+              </h3>
+              <div className="h-80">
                 <DropZone ref={modalFileInput} />
-                <div className="mt-10 flex flex-row">
-                  <Button onClick={() => uploadFile()}>Subir</Button>
-                </div>
               </div>
-            </Modal>
-          )}
-        </>
-      </Wrapper>
-    </>
+              <div className="flex flex-col justify-end items-center">
+                <div className="my-2 w-2/5">
+                  <Button className="btn-primary" onClick={() => uploadFile()}>
+                    Subir
+                  </Button>
+                </div>
+                {uploadFileURL && (
+                  <>
+                    <div className="my-2 w-2/5">
+                      <button
+                        className="btn-secondary"
+                        onClick={() => openFileInNewTab()}
+                      >
+                        Ver evidencia
+                      </button>
+                    </div>
+                    <div className="flex items-center justify-center my-5">
+                      <AiOutlineFileProtect
+                        size={40}
+                        className=" text-gnp-blue-600"
+                      />
+                      <p className="ml-3 text-sm font-semibold">
+                        {fileName?.length > 30
+                          ? `${fileName?.slice(0, 40)}...`
+                          : fileName}
+                      </p>
+                    </div>
+                  </>
+                )}
+              </div>
+            </div>
+          </Modal>
+        )}
+      </>
+    </Wrapper>
   );
 }
 
