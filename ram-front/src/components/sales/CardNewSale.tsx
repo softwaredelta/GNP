d8fbc58bc401a3661bcf9284ae11a963282c1c5b
Monica <a01707439@itesm.mx>
2023-04-24 15:59:16 -0600

    [FRONT] Responsive card fix

--- ram-front/src/components/sales/CardNewSale.tsx
+++ ram-front/src/components/sales/CardNewSale.tsx
@@ -22,8 +22,6 @@ const CardNewSale = ({ assuranceTypes }: IListAssuranceTypesProps) => {
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-
-    // Aquí se puede agregar la lógica para enviar los datos al servidor
   };
 
   const { response, error, loading, callback } = useAxios<{
@@ -49,154 +47,103 @@ const CardNewSale = ({ assuranceTypes }: IListAssuranceTypesProps) => {
     },
   });
 
-  // async function createNewSale() {
-  //   try {
-
-  //     const { data, status } = await axios.post<CreateSaleResponse>(
-  //       `http://localhost:8080/sales/create`,
-  //       {
-  //         policyNumber: "73456",
-  //         sellDate: "12/03/2022",
-  //         assuranceType: {
-  //           name: "test-assurance-type-1",
-  //           description: "test-assurance-type-1-description",
-  //           id: "test-at-1",
-  //         },
-  //         amountInCents: "123456",
-  //         clientName: "Renato",
-  //       },
-  //       {
-  //         headers: {
-  //           "Content-Type": "application/json",
-  //           Accept: "application/json",
-  //         },
-  //       },
-  //     );
-  //     console.log(JSON.stringify(data, null, 4));
-  //     console.log(status);
-  //     return data;
-  //   } catch (error) {
-  //     if (axios.isAxiosError(error)) {
-  //       console.log("error message: ", error.message);
-  //       // 👇️ error: AxiosError<any, any>
-  //       return error.message;
-  //     } else {
-  //       console.log("unexpected error: ", error);
-  //       return "An unexpected error occurred";
-  //     }
-  //   }
-  // }
-
   return (
-    <div className="rounded-lg flex-col md:flex-row w-full md:w-3/4 bg-gradient-to-t from-gnp-gray-ligth to-gnp-cream shadow-lg flex">
+    <div className="grid grid-cols-4 bg-gradient-to-t from-gnp-cream to-gnp-white rounded-lg m-4">
       <img
         src={moneyGrowth}
         alt="Imagen"
-        className="rounded-l-lg object-cover lg:h-full sm:h-full h-full md:h-full "
+        className="rounded-l-lg object-cover hidden md:block md:h-full"
       />
-      <div className="w-full md:w-3/4 h-full">
-        <div className="bg-orange-500 p-4 flex justify-center items-center rounded-l-lg">
+      <div className="col-span-4 w-96 md:w-full md:col-span-3 rounded-l-lg grid grid-cols">
+        <div className="col bg-orange-500 flex justify-center p-4 items-center rounded-tr-lg">
           <h1 className="text-3xl my-2 font-bold text-white">Agregar venta</h1>
         </div>
-        <div className="p-4">
-          <form onSubmit={handleSubmit} className="flex flex-wrap">
-            <div className="w-full md:w-1/2 md:p-2">
-              <label className="block text-gray-700 ml-3 text-lg font-bold mb-1">
-                Poliza
-              </label>
-              <input
-                className="input-primary w-full"
-                type="text"
-                placeholder="Ingrese el número de poliza"
-                value={poliza}
-                onChange={(e: ChangeEvent<HTMLInputElement>) =>
-                  setPoliza(e.target.value)
-                }
-              />
-            </div>
-            <div className="w-full md:w-1/2 md:p-2">
-              <label
-                htmlFor="datePicker"
-                className="block text-gray-700 ml-3 text-lg font-bold mb-1"
+        <div className="col grid grid-cols-4 gap-4">
+          <div className="md:col-span-2 col-span-4 px-8 pt-8">
+            <label className="block text-gray-700 ml-3 text-lg font-bold mb-1">
+              Poliza
+            </label>
+            <input
+              className="input-primary w-full"
+              type="text"
+              placeholder="Ingrese el número de poliza"
+              value={poliza}
+              onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                setPoliza(e.target.value)
+              }
+            />
+            <label className="block text-gray-700 ml-3 text-lg font-bold mb-1">
+              Monto
+            </label>
+            <input
+              className="input-primary w-full"
+              type="number"
+              placeholder="Ingrese el monto de la venta"
+              value={monto}
+              onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                setMonto(e.target.value)
+              }
+            />
+            <label className="block text-gray-700 ml-3 text-lg font-bold mb-1">
+              Nombre del Cliente
+            </label>
+            <input
+              className="input-primary w-full"
+              type="text"
+              data-testid="clientInput"
+              placeholder="Ingrese el nombre del cliente"
+              value={client}
+              onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                setClient(e.target.value)
+              }
+            />
+          </div>
+          <div className="md:col-span-2 col-span-4 px-8 pt-8">
+            <label
+              htmlFor="datePicker"
+              className="block text-gray-700 ml-3 text-lg font-bold mb-1"
+            >
+              Fecha
+            </label>
+            <DatePicker
+              selected={selectedDate}
+              name="datePicker"
+              id="datePicker"
+              onChange={(date: Date | null) => setSelectedDate(date)}
+              dateFormat="dd/MM/yyyy"
+              className="input-primary w-full"
+              placeholderText="dd/mm/aaaa"
+            />
+            <label className="block text-gray-700 ml-3 text-lg font-bold mb-1">
+              Tipo de seguro
+            </label>
+            <select className="input-primary w-full">
+              {assuranceTypes.map((at, index) => (
+                <option key={index} value={at.name}>
+                  {at.name}
+                </option>
+              ))}
+            </select>
+            <label className="block text-gray-700 ml-3 text-lg font-bold mb-1">
+              Periodicidad
+            </label>
+            <select className="input-primary w-full">
+              <option> semestral </option>
+              <option> mensual </option>
+              <option> anual </option>
+            </select>
+          </div>
+          <div className="col-span-4 flex justify-center items-center pb-8">
+            <div className="w-52">
+              <button
+                className="btn-primary flex justify-center items-center h-12"
+                onClick={callback}
               >
-                Fecha
-              </label>
-              <DatePicker
-                selected={selectedDate}
-                name="datePicker"
-                id="datePicker"
-                onChange={(date: Date | null) => setSelectedDate(date)}
-                dateFormat="dd/MM/yyyy"
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              />
+                <span className="font-semibold text-lg"> Enviar </span>
+                <TbSend size={20} className="ml-2" />
+              </button>
             </div>
-            <div className="w-full md:w-1/2 md:p-2">
-              <label className="block text-gray-700 ml-3 text-lg font-bold mb-1">
-                Monto
-              </label>
-              <input
-                className="input-primary w-full"
-                type="number"
-                placeholder="Ingrese el monto de la venta"
-                value={monto}
-                onChange={(e: ChangeEvent<HTMLInputElement>) =>
-                  setMonto(e.target.value)
-                }
-              />
-            </div>
-            <div className="w-full md:w-1/2 md:p-2">
-              <label className="block text-gray-700 ml-3 text-lg font-bold mb-1">
-                Nombre del Cliente
-              </label>
-              <input
-                className="input-primary w-full"
-                type="text"
-                data-testid="clientInput"
-                placeholder="Ingrese el nombre del cliente"
-                value={client}
-                onChange={(e: ChangeEvent<HTMLInputElement>) =>
-                  setClient(e.target.value)
-                }
-              />
-            </div>
-            <div className="w-full md:w-1/2 md:p-2">
-              <label className="block text-gray-700 ml-3 text-lg font-bold mb-1">
-                Adjuntar evidencia
-              </label>
-              <input
-                className="input-primary w-full"
-                type="text"
-                placeholder="Ingrese el nombre del cliente"
-                value={client}
-                onChange={(e: ChangeEvent<HTMLInputElement>) =>
-                  setClient(e.target.value)
-                }
-              />
-            </div>
-            <div className="w-full md:w-1/2 md:p-2">
-              <label className="block text-gray-700 ml-3 text-lg font-bold mb-1">
-                Tipo de seguro
-              </label>
-              <select>
-                {assuranceTypes.map((at, index) => (
-                  <option key={index} value={at.name}>
-                    {at.name}
-                  </option>
-                ))}
-              </select>
-            </div>
-            <div className="w-full flex items-center justify-center">
-              <div className="w-48">
-                <button
-                  className="btn-primary flex justify-center items-center h-12"
-                  onClick={callback}
-                >
-                  <span className="font-semibold text-lg"> Enviar </span>
-                  <TbSend size={20} className="ml-2" />
-                </button>
-              </div>
-            </div>
-          </form>
+          </div>
         </div>
       </div>
     </div>
