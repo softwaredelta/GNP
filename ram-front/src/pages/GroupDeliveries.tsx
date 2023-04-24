8407a04f4fc273f1818f973f38971f0b3d83e790
alfredohuerta <eagarciahuerta@hotmail.com>
2023-04-24 09:31:21 -0600

    [FRONT] Conected frontend of Manager deliveries with api

--- ram-front/src/pages/GroupDeliveries.tsx
+++ ram-front/src/pages/GroupDeliveries.tsx
@@ -2,32 +2,28 @@
 
 import DeliveryCard from "../components/generics/cards/base/DeliveryCard";
 import ManagerDelivery from "../components/generics/cards/info/ManagerDelivery";
-import ManagerDeliveryCard from "../components/generics/cards/info/ManagerDelivery";
 import Wrapper from "../containers/Wrapper";
-import { useAuthentication } from "../lib/api/api-auth";
-import AgentDelivery, {
-  AgentDeliveryProps,
-} from "../components/generics/cards/info/AgentDelivery";
-//import { allCourses$ } from "../lib/api/api-courses";
+import { allCourses$ } from "../lib/api/api-courses";
+import { useRecoilValue } from "recoil";
 
 export default function GroupDeliveries() {
-  //const groups = useRecoilValue(allCourses$);
+  const groups = useRecoilValue(allCourses$);
 
   return (
     <div>
       <Wrapper>
         <>
-          <p className="title">Nombre del grupo</p>
+          <p className="title">Nombre del curso</p>
           <div className="w-full flex justify-center min-h-[26%] gap-10">
             <div className="w-3/5">
               <div className="pb-6">
-              <DeliveryCard
-                color="blue"
-                nameDelivery="Nombre de la entrega"
-                image="https://i.blogs.es/799a0e/ydray-mew_27_articuno_45l_hyperx_environment_front/1366_2000.jpeg"
-              >
-                <ManagerDelivery membersNumber={30}></ManagerDelivery>
-              </DeliveryCard>
+                <DeliveryCard
+                  color="blue"
+                  nameDelivery="Nombre de la entrega"
+                  image="https://i.blogs.es/799a0e/ydray-mew_27_articuno_45l_hyperx_environment_front/1366_2000.jpeg"
+                >
+                  <ManagerDelivery membersNumber={30}></ManagerDelivery>
+                </DeliveryCard>
               </div>
             </div>
           </div>
