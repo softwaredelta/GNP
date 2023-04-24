5dc26325dfda93c0bfa478355384ed2a804f9559
Kenny Vercaemer <vercaemereduard@gmail.com>
2023-04-24 17:23:48 -0600

    show login alert

--- ram-front/src/pages/Login.tsx
+++ ram-front/src/pages/Login.tsx
@@ -1,10 +1,29 @@
 // (c) Delta Software 2023, rights reserved.
 
 import { LoginScreen } from "../components/auth/Login";
+import useAlert from "../hooks/useAlert";
 import { useAuthentication } from "../lib/api/api-auth";
+import { useEffect } from "react";
 
 export default function Login() {
-  const { authenticate } = useAuthentication();
+  const { authenticate, isLoading, hasError, clearError } = useAuthentication();
+  const { showAlert } = useAlert();
 
-  return <LoginScreen onLogin={authenticate}></LoginScreen>;
+  useEffect(() => {
+    if (hasError) {
+      showAlert(
+        {
+          type: "error",
+          message: "Error de login",
+          description: "Credenciales introducidas no válidas",
+        },
+        3,
+      );
+      clearError();
+    }
+  }, [hasError, clearError, showAlert]);
+
+  return (
+    <LoginScreen onLogin={authenticate} isLoading={isLoading}></LoginScreen>
+  );
 }
