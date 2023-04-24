176ae939c0b069dada3853bcb3e4c5650a78a62e
Monica <a01707439@itesm.mx>
2023-04-24 13:15:59 -0600

    [FRONT] Changes to filters

--- ram-front/src/routes/privates.ts
+++ ram-front/src/routes/privates.ts
@@ -4,10 +4,15 @@ import { lazy } from "react";
 import { IRoute } from "../types";
 
 const Home = lazy(async () => import("../pages/Home"));
+const SalesHistory = lazy(async () => import("../pages/SalesHistory"));
 
 export const privateRoutes: IRoute[] = [
   {
     path: "/",
     component: Home,
+  },
+  {
+    path: "/sales-history",
+    component: SalesHistory,
   },
 ];
