d071fa18b87701649da2f20153156607d0f9b32f
Kenny Vercaemer <vercaemereduard@gmail.com>
2023-04-28 14:21:55 -0600

    fix: auth init loading

--- ram-front/src/App.tsx
+++ ram-front/src/App.tsx
@@ -1,11 +1,32 @@
 // (c) Delta Software 2023, rights reserved.
 
 import React from "react";
-import { RecoilRoot } from "recoil";
+import { RecoilRoot, useRecoilValue } from "recoil";
 import { BrowserRouter } from "react-router-dom";
 import { ErrorBoundary } from "react-error-boundary";
-import { AuthenticationHandler } from "./lib/api/api-auth";
+import {
+  AuthenticationHandler,
+  AuthenticationInitializationHandler,
+  isAuthenticationReady$,
+} from "./lib/api/api-auth";
 import { HashProvider } from "./lib/api/api-hash";
+
+// makes sure app is only rendered once auth initialization is complete
+export function AuthWrapper({ children }: { children: React.ReactNode }) {
+  const isAuthenticationReady = useRecoilValue(isAuthenticationReady$);
+
+  return (
+    <>
+      <AuthenticationInitializationHandler />
+      {isAuthenticationReady && (
+        <>
+          <AuthenticationHandler />
+          {children}
+        </>
+      )}
+    </>
+  );
+}
 
 export function App({
   hash,
@@ -15,7 +36,7 @@ export function App({
   children?: React.ReactNode;
 }) {
   return (
-    <React.Suspense fallback={<p>loading site...</p>}>
+    <React.Suspense fallback={<p></p>}>
       <ErrorBoundary
         fallbackRender={({ error, resetErrorBoundary }) => (
           <div role="alert">
@@ -28,8 +49,7 @@ export function App({
         <RecoilRoot>
           <BrowserRouter>
             <HashProvider value={hash}>
-              <AuthenticationHandler />
-              {children}
+              <AuthWrapper>{children}</AuthWrapper>
             </HashProvider>
           </BrowserRouter>
         </RecoilRoot>
