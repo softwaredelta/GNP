d63fb44b95deebc5269dd0feacea5fe9b1a2d953
Kenny Vercaemer <vercaemereduard@gmail.com>
2023-04-28 17:45:54 -0600

    add: action buttons

--- ram-front/src/components/deliverables/UserDeliveryRow.tsx
+++ ram-front/src/components/deliverables/UserDeliveryRow.tsx
@@ -3,7 +3,29 @@
 import { Table } from "flowbite-react";
 import { IUserDelivery } from "../../types";
 import { useMemo } from "react";
-import { FaTrash } from "react-icons/fa";
+import { ImCross, ImCheckmark } from "react-icons/im";
+import { type IconType } from "react-icons/lib";
+
+const actionIconSize = 20;
+
+function ActionButton({
+  color,
+  Icon,
+  onClick,
+}: {
+  color: string;
+  Icon: IconType;
+  onClick: () => void;
+}) {
+  return (
+    <button onClick={onClick}>
+      <Icon
+        className={`${color} hover:scale-125 transition-all ease-in-out duration-100`}
+        size={actionIconSize}
+      />
+    </button>
+  );
+}
 
 export function UserDeliveryRow({
   dateDelivery,
@@ -49,8 +71,23 @@ export function UserDeliveryRow({
       <Table.Cell>{status}</Table.Cell>
       <Table.Cell>Archivo</Table.Cell>
       <Table.Cell>{date}</Table.Cell>
-      <Table.Cell>
-        <FaTrash className="text-red-500" />
+      <Table.Cell className="items-center justify-center h-full">
+        <div className="flex flex-row gap-5 justify-around">
+          <ActionButton
+            color="text-green-500"
+            Icon={ImCheckmark}
+            onClick={() => {
+              alert("Aceptando...");
+            }}
+          />
+          <ActionButton
+            color="text-red-500"
+            Icon={ImCross}
+            onClick={() => {
+              alert("Rechazando...");
+            }}
+          />
+        </div>
       </Table.Cell>
     </Table.Row>
   );
