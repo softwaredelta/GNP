c449ad77e3212d923e0f2e9e54e6ee0a34e4c293
Kenny Vercaemer <vercaemereduard@gmail.com>
2023-05-11 16:53:10 -0600

    add: create sell endpoint

--- ram-front/src/components/sales/CardNewSale.tsx
+++ ram-front/src/components/sales/CardNewSale.tsx
@@ -9,20 +9,29 @@ import useAxios from "../../hooks/useAxios";
 import Swal from "sweetalert2";
 import { useForm } from "react-hook-form";
 import { IAssuranceType } from "../../types";
+import { FileInput } from "flowbite-react";
+
 export interface IListAssuranceTypesProps {
   assuranceTypes: IAssuranceType[];
 }
 
 const CardNewSale = ({ assuranceTypes }: IListAssuranceTypesProps) => {
   const [paidDate, setPaidDate] = useState<Date | null>(null);
+  const [emissionDate, setEmissionDate] = useState<Date | null>(null);
+
+  const [file, setFile] = useState<File | null>(null);
 
   type FormValues = {
     policyNumber: number;
+    assuranceTypeId: string;
+    userId: string;
+    paidDate: Date;
     yearlyFee: number;
     contractingClient: string;
-    assuranceTypeId: string;
     periodicity: string;
-    paidDate: Date;
+    paidFee: number;
+    insuredCostumer: string;
+    emissionDate: Date;
   };
 
   const { register, handleSubmit, reset } = useForm<FormValues>();
@@ -30,17 +39,50 @@ const CardNewSale = ({ assuranceTypes }: IListAssuranceTypesProps) => {
   const { response, error, callback } = useAxios({
     url: "sales/create",
     method: "POST",
-    body: {},
   });
 
   const sendData = (data: FormValues) => {
-    if (callback) {
-      callback({
-        ...data,
-        paidDate: paidDate,
+    if (file) {
+      const formData: FormData = new FormData();
+      formData.append("file", file);
+      formData.append("policyNumber", data.policyNumber.toString());
+      formData.append("assuranceTypeId", data.assuranceTypeId.toString());
+      formData.append("userId", data.userId);
+      formData.append("paidDate", paidDate?.toString() as string);
+      formData.append("yearlyFee", data.yearlyFee.toString());
+      formData.append("contractingClient", data.contractingClient);
+      formData.append("periodicity", data.periodicity);
+      formData.append("paidFee", data.paidFee.toString());
+      formData.append("insuredCostumer", data.insuredCostumer);
+      formData.append("emissionDate", emissionDate?.toString() as string);
+
+      try {
+        callback?.(formData);
+      } catch (err) {
+        console.error(err);
+      }
+    } else {
+      Swal.fire({
+        title: "Error!",
+        text: `No seleccionaste archivo.`,
+        icon: "error",
+        confirmButtonText: "OK",
       });
     }
   };
+
+  // const sendData = (data: FormValues) => {
+
+  //   if (callback) {
+  //     callback({
+  //       ...data,
+  //       paidDate: paidDate,
+  //       emissionDate: emissionDate,
+
+  //     });
+  //   }
+  //   }
+  // };
 
   useEffect(() => {
     if (response) {
@@ -55,9 +97,13 @@ const CardNewSale = ({ assuranceTypes }: IListAssuranceTypesProps) => {
         contractingClient: "",
         assuranceTypeId: "1",
         periodicity: "Mensual",
+        paidFee: 0,
+        insuredCostumer: "",
       });
       setPaidDate(new Date());
+      setEmissionDate(new Date());
     } else if (error) {
+      console.log({ error });
       Swal.fire({
         title: "Error!",
         text: `Ocurrió un error al registrar la venta.\n
@@ -69,18 +115,19 @@ const CardNewSale = ({ assuranceTypes }: IListAssuranceTypesProps) => {
   }, [response, error]);
 
   return (
-    <div className="m-4 grid w-3/4 grid-cols-4 rounded-lg bg-gradient-to-t from-[#DBDFE6] to-[#FFEFDB] shadow-lg">
+    <div className="m-4 grid w-11/12 grid-cols-4 rounded-lg bg-gradient-to-t from-[#DBDFE6] to-[#FFEFDB] shadow-lg">
       <img
         src={moneyGrowth}
         alt="Imagen"
         className="hidden rounded-l-lg object-cover md:block md:h-full"
       />
-      <div className="grid-cols col-span-4 grid w-full rounded-l-lg md:col-span-3 md:w-full">
+      <div className="grid-cols col-span-3 grid w-full rounded-l-lg md:col-span-3 md:w-full">
         <div className="col flex items-center justify-center rounded-t-lg bg-orange-500 p-4 md:rounded-tr-lg">
           <h1 className="my-2 text-3xl font-bold text-white">Agregar venta</h1>
         </div>
-        <div className="col grid grid-cols-4 gap-4">
-          <div className="col-span-4 px-8 pt-8 md:col-span-2">
+        <div className="col grid grid-cols-3">
+          <div className="col-span-1 px-6 pt-8 md:col-span-1">
+            {/* 1st column */}
             <label className="ml-3 mb-1 block text-lg font-bold text-gray-700">
               Póliza
             </label>
@@ -101,14 +148,69 @@ const CardNewSale = ({ assuranceTypes }: IListAssuranceTypesProps) => {
               })}
             />
             <label className="ml-3 mb-1 block text-lg font-bold text-gray-700">
-              Monto
+              Nombre del asegurado
+            </label>
+            <input
+              className="input-primary w-full"
+              placeholder="Ingrese el nombre de asegurado"
+              {...register("insuredCostumer", {
+                required: "El campo Nombre de Asegurado es requerido",
+                minLength: {
+                  value: 3,
+                  message: "El nombre debe tener al menos 3 caracteres",
+                },
+                maxLength: {
+                  value: 50,
+                  message: "El nombre debe tener máximo 50 caracteres",
+                },
+              })}
+            />
+            <label className="ml-3 mb-1 block text-lg font-bold text-gray-700">
+              Nombre del Contratante
+            </label>
+            <input
+              className="input-primary w-full"
+              placeholder="Ingrese el nombre del contratante"
+              {...register("contractingClient", {
+                required: "El campo Nombre del contratante es requerido",
+                minLength: {
+                  value: 3,
+                  message: "El nombre debe tener al menos 3 caracteres",
+                },
+                maxLength: {
+                  value: 50,
+                  message: "El nombre debe tener máximo 50 caracteres",
+                },
+              })}
+            />
+          </div>
+          {/* 2nd column */}
+          <div className="col-span-1 px-6 pt-8 md:col-span-1">
+            <label
+              htmlFor="datePicker"
+              className="ml-3 mb-1 block text-lg font-bold text-gray-700"
+            >
+              Fecha Emisión
+            </label>
+            <DatePicker
+              selected={emissionDate}
+              name="datePicker"
+              id="datePicker"
+              onChange={(date: Date | null) => setEmissionDate(date)}
+              dateFormat="dd/MM/yyyy"
+              className="input-primary w-full"
+              placeholderText="dd/mm/aaaa"
+              required
+            />
+            <label className="ml-3 mb-1 block text-lg font-bold text-gray-700">
+              Prima Anual
             </label>
             <input
               className="input-primary w-full"
               type="number"
-              placeholder="Ingrese el monto de la venta"
+              placeholder="Ingrese la prima anual"
               {...register("yearlyFee", {
-                required: "El campo monto de venta es requerido",
+                required: "El campo de prima anual es requerido",
                 minLength: {
                   value: 2,
                   message: "El monto debe tener al menos 2 dígitos",
@@ -124,30 +226,24 @@ const CardNewSale = ({ assuranceTypes }: IListAssuranceTypesProps) => {
               })}
             />
             <label className="ml-3 mb-1 block text-lg font-bold text-gray-700">
-              Nombre del Cliente
+              Periodicidad
             </label>
-            <input
+            <select
               className="input-primary w-full"
-              placeholder="Ingrese el nombre del cliente"
-              {...register("contractingClient", {
-                required: "El campo Nombre del cliente es requerido",
-                minLength: {
-                  value: 3,
-                  message: "El nombre debe tener al menos 3 caracteres",
-                },
-                maxLength: {
-                  value: 50,
-                  message: "El nombre debe tener máximo 50 caracteres",
-                },
+              {...register("periodicity", {
+                required: "El campo periodicidad es requerido",
               })}
-            />
-          </div>
-          <div className="col-span-4 px-8 pt-8 md:col-span-2">
-            <label
-              htmlFor="datePicker"
-              className="ml-3 mb-1 block text-lg font-bold text-gray-700"
             >
-              Fecha
+              <option value={"Mensual"}> Mensual </option>
+              <option value={"Trimestral"}> Trimestral </option>
+              <option value={"Semestral"}> Semestral </option>
+              <option value={"Anual"}> Anual </option>
+            </select>
+          </div>
+          {/* 3rd column */}
+          <div className="col-span-1 px-6 pt-8 md:col-span-1">
+            <label className="ml-3 mb-1 block text-lg font-bold text-gray-700">
+              Fecha Pago
             </label>
             <DatePicker
               selected={paidDate}
@@ -160,7 +256,30 @@ const CardNewSale = ({ assuranceTypes }: IListAssuranceTypesProps) => {
               required
             />
             <label className="ml-3 mb-1 block text-lg font-bold text-gray-700">
-              Tipo de seguro
+              Prima Pagada
+            </label>
+            <input
+              className="input-primary w-full"
+              type="number"
+              placeholder="Ingrese la prima pagada"
+              {...register("paidFee", {
+                required: "El campo prima pagada es requerido",
+                minLength: {
+                  value: 2,
+                  message: "El monto debe tener al menos 2 dígitos",
+                },
+                maxLength: {
+                  value: 7,
+                  message: "El monto debe tener máximo 7 dígitos",
+                },
+                min: {
+                  value: 1,
+                  message: "El monto debe ser mayor a 0",
+                },
+              })}
+            />
+            <label className="ml-3 mb-1 block text-lg font-bold text-gray-700">
+              Tipo Seguro
             </label>
             <select
               className="input-primary w-full"
@@ -174,39 +293,37 @@ const CardNewSale = ({ assuranceTypes }: IListAssuranceTypesProps) => {
                 </option>
               ))}
             </select>
-            <label className="ml-3 mb-1 block text-lg font-bold text-gray-700">
-              Periodicidad
-            </label>
-            <select
-              className="input-primary w-full"
-              {...register("periodicity", {
-                required: "El campo periodicidad es requerido",
-              })}
-            >
-              <option value={"Semestral"}> Semestral </option>
-              <option value={"Mensual"}> Mensual </option>
-              <option value={"Anual"}> Anual </option>
-            </select>
           </div>
 
-          <div className="col-span-4 flex items-center justify-center pb-8">
-            <div className="w-52">
-              <button
-                className="btn-primary flex h-12 items-center justify-center"
-                onClick={handleSubmit(sendData, (errorsFields) => {
-                  Swal.fire({
-                    title: "Error!",
-                    text: `Ocurrió un error al registrar la venta.\n
+          {/* 4th column */}
+          <div className="col-span-2 mb-4 px-6 pt-8 ">
+            {/* File zone */}
+            <FileInput
+              id="file"
+              onChange={(e) => {
+                if (e.target.files) {
+                  setFile(e.target.files[0]);
+                } else setFile(null);
+              }}
+              helperText="Sube la evidencia de tu venta"
+            />
+          </div>
+          <div className="col-span-1 px-6 pt-8 md:col-span-1">
+            <button
+              className="btn-primary flex h-12 items-center justify-center"
+              onClick={handleSubmit(sendData, (errorsFields) => {
+                Swal.fire({
+                  title: "Error!",
+                  text: `Ocurrió un error al registrar la venta.\n
                     ${Object.values(errorsFields).map((e) => e.message + " ")}`,
-                    icon: "error",
-                    confirmButtonText: "OK",
-                  });
-                })}
-              >
-                <span className="text-lg font-semibold"> Enviar </span>
-                <TbSend size={20} className="ml-2" />
-              </button>
-            </div>
+                  icon: "error",
+                  confirmButtonText: "OK",
+                });
+              })}
+            >
+              <span className="text-lg font-semibold"> Enviar </span>
+              <TbSend size={20} className="ml-2" />
+            </button>
           </div>
         </div>
       </div>
