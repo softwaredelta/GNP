895cae9cd17c608d2897b06218812ed761481b3e
Ren <A01275401@itesm.mx>
2023-05-11 18:11:13 -0600

    rebase develop

--- ram-back/src/controller/prospect.ts
+++ ram-back/src/controller/prospect.ts
@@ -11,23 +11,23 @@ import { RequestHandler } from "express";
 export const prospectsRouter = Router();
 
 
-const updateParameters = j.object({
-    userId: j.string().required(),
-    statusChange: j.boolean().required(),
-  });
+// const updateParameters = j.object({
+//     userId: j.string().required(),
+//     statusChange: j.boolean().required(),
+//   });
 
-  prospectsRouter.get("/agents", authMiddleware(), async (req, res) => {
-    if (!req.user) {
-      res.status(401).json({ message: "No user" });
-      return;
-    }
-    const userId = req.user.id;
-    const db = await getDataSource();
-    const sales = await db.manager.find(SellEnt, {
-      relations: { assuranceType: true, user: true },
-      where: { userId },
-    });
+//   prospectsRouter.get("/agents", authMiddleware(), async (req, res) => {
+//     if (!req.user) {
+//       res.status(401).json({ message: "No user" });
+//       return;
+//     }
+//     const userId = req.user.id;
+//     const db = await getDataSource();
+//     const sales = await db.manager.find(SellEnt, {
+//       relations: { assuranceType: true, user: true },
+//       where: { userId },
+//     });
   
-    res.json(sales);
-  });
+//     res.json(sales);
+//   });
   
