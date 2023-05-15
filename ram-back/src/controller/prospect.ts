668b502e0ec3d13d9188b1d9696b2b154e5f5781
ulmo369 <a01366802@itesm.mx>
2023-05-15 17:26:55 -0600

    [BACK] Add endpoint and test from get prospects

--- ram-back/src/controller/prospect.ts
+++ ram-back/src/controller/prospect.ts
@@ -4,7 +4,7 @@ import { RequestHandler, Router } from "express";
 export const prospectRouter = Router();
 import * as j from "joi";
 import { authMiddleware } from "./user";
-import { createProspect } from "../app/prospect";
+import { createProspect, getProspectStatus } from "../app/prospect";
 
 const prospectParameters = j.object({
   name: j.string().required(),
@@ -52,3 +52,23 @@ prospectRouter.post(
     res.status(201).json(prospect);
   },
 );
+
+prospectRouter.get("/my-prospects", authMiddleware(), async (req, res) => {
+  const { user } = req;
+  if (!user) {
+    res.status(401).json({ message: "No user" });
+    return;
+  }
+
+  const userId = user.id;
+
+  const { prospects, error, reason } = await getProspectStatus({ userId });
+
+  if (error) {
+    res.status(400).json({ message: error, reason });
+    return;
+  }
+
+  res.status(200).json({ prospects });
+  return;
+});
