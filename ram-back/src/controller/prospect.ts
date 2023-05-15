5c52b00325a6c32759f1c81230032e4100e9f5e9
Monica <a01707439@itesm.mx>
2023-05-15 16:08:21 -0600

    [BACK] Minor fixes

--- ram-back/src/controller/prospect.ts
+++ ram-back/src/controller/prospect.ts
@@ -7,8 +7,6 @@ import { authMiddleware } from "./user";
 import { createProspect } from "../app/prospect";
 import { getDataSource } from "../arch/db-client";
 import { ProspectEnt } from "../entities/prospect.entity";
-// import { StatusNames } from "../entities/status.entity";
-// import { ProspectStatusEnt } from "../entities/prospect-status.entity";
 
 const prospectParameters = j.object({
   name: j.string().required(),
@@ -59,16 +57,10 @@ prospectRouter.post(
 prospectRouter.get("/count-prospects-new/:id", async (req, res) => {
   const db = await getDataSource();
   const id = req.params.id;
-  // const status = StatusNames.NEW;
-  const sales = await db.manager.count(ProspectEnt, {
-    // relations: ["prospect_status", "prospect_status.prospect"],
+  const agents = await db.manager.count(ProspectEnt, {
     where: {
       userId: id,
-      // prospectStatus: StatusNames.NEW,
-      // prospect_status:{
-      //   statusId: StatusNames.NEW
-      // }
     },
   });
-  res.json(sales);
+  res.json(agents);
 });
