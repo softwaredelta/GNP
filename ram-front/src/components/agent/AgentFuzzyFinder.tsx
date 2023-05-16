f85e2fbcff6f80d847f503e64d26ea3c717069d1
Kenny Vercaemer <vercaemereduard@gmail.com>
2023-05-16 13:24:50 -0600

    key handler

--- ram-front/src/components/agent/AgentFuzzyFinder.tsx
+++ ram-front/src/components/agent/AgentFuzzyFinder.tsx
@@ -7,29 +7,34 @@ import {
 } from "../../lib/api/api-agent-fuzzy-finder";
 import { useCallback, useState } from "react";
 import { useLoadable } from "../../lib/loadable";
+import { IUser } from "../../types";
 
 const TIMER_DELAY = 300;
 
-function UsersList() {
-  const [users /*, state*/] = useLoadable([], fuzzyFinderUsers$);
+function FuzzyFinderUser({
+  selected,
+  user,
+}: {
+  selected: boolean;
+  user: IUser;
+}) {
+  const selectedClass = selected ? "bg-blue-500 border-blue-700 border-1" : "";
 
   return (
-    <>
-      {/* <p>{state}</p> */}
-      {users.map((user) => (
-        <div key={user.id}>
-          {user.name} {user.lastName}
-        </div>
-      ))}
-    </>
+    <div className={`${selectedClass}`}>
+      {user.name} {user.lastName}
+    </div>
   );
 }
 
 export default function AgentFuzzyFinder() {
   const setQuery = useSetRecoilState(fuzzyFinderQuery$);
+  const [users /*, state*/] = useLoadable([], fuzzyFinderUsers$);
   const [timer, setTimer] = useState<null | ReturnType<typeof setTimeout>>(
     null,
   );
+  const [selectedItem, setSelectedItem] = useState<null | number>(null);
+
   const updateQuery = useCallback(
     (query: string) => {
       if (timer) {
@@ -38,16 +43,57 @@ export default function AgentFuzzyFinder() {
       const newTimer = setTimeout(() => {
         setQuery(query);
         setTimer(null);
+        setSelectedItem(null);
       }, TIMER_DELAY);
       setTimer(newTimer);
     },
     [setQuery, timer],
   );
 
+  const handleKeyDown = useCallback(
+    (event: any) => {
+      if (event.key === "ArrowUp") {
+        event.preventDefault();
+        // Move the selection up
+        const selectedIndex =
+          selectedItem !== null ? (selectedItem > 0 ? selectedItem - 1 : 0) : 0;
+        setSelectedItem(selectedIndex);
+      } else if (event.key === "ArrowDown") {
+        event.preventDefault();
+        // Move the selection down
+        const selectedIndex =
+          selectedItem !== null
+            ? selectedItem < users.length - 1
+              ? selectedItem + 1
+              : users.length - 1
+            : 0;
+        setSelectedItem(selectedIndex);
+      } else if (event.key === "Enter") {
+        event.preventDefault();
+        // Handle selection on pressing Enter
+        if (selectedItem !== null) {
+          // console.log({ selected: users[selectedItem] });
+        }
+      }
+    },
+    [selectedItem, users],
+  );
+
   return (
     <>
-      <input type="text" onChange={(e) => updateQuery(e.target.value)} />
-      <UsersList />
+      <input
+        type="text"
+        onChange={(e) => updateQuery(e.target.value)}
+        onKeyDown={handleKeyDown}
+      />
+      {/* <p>{state}</p> */}
+      {users.map((user, index) => (
+        <FuzzyFinderUser
+          key={user.id}
+          selected={selectedItem !== null && selectedItem === index}
+          user={user}
+        />
+      ))}
     </>
   );
 }
