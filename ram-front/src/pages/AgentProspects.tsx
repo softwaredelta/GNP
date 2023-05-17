9978cb15b80ce0a61f3231b3bf3172dd75d90659
iangg29 <iangarciag@outlook.com>
2023-05-16 23:08:41 -0600

    [FRONTEND] Added Agent prospects page.

--- ram-front/src/pages/AgentProspects.tsx
+++ ram-front/src/pages/AgentProspects.tsx
@@ -1,13 +1,20 @@
 // (c) Delta Software 2023, rights reserved.
 
 import Wrapper from "../containers/Wrapper";
-import FunnelProspect from "../components/prospects/FunnelTable";
-import { IUser } from "../types";
+import { IProspect, IUser } from "../types";
 import useAxios from "../hooks/useAxios";
+import { useParams } from "react-router-dom";
 
 export default function AgentProspects() {
-  const { response: agents } = useAxios<IUser[]>({
-    url: `user/all-agents`,
+  const { id } = useParams();
+
+  const {
+    response,
+  }: { response: { agent: IUser; prospects: IProspect[] } | null } = useAxios<{
+    agent: IUser;
+    prospects: IProspect[];
+  } | null>({
+    url: `user/prospect/agent/${id}`,
     method: "GET",
   });
 
@@ -16,12 +23,10 @@ export default function AgentProspects() {
       <div>
         <div className="flex w-full items-center justify-start pt-8">
           <h1 className=" rounded-r-2xl bg-gnp-orange-500 py-3 px-20 text-xl font-bold text-white">
-            Prospectos
+            Prospectos para {response?.agent.name}
           </h1>
         </div>
-        <div className="mt-8 flex flex-col items-center justify-center">
-          {agents && <FunnelProspect agents={agents} />}
-        </div>
+        <div className="mt-8 flex flex-col items-center justify-center"></div>
       </div>
     </Wrapper>
   );
