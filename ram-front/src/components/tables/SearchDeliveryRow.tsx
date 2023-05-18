0abac7d5ffdf134c71b306a674dc7a0eb85016c7
Fermin Mendez <A01703366@itesm.mx>
2023-05-17 21:07:40 -0600

    [FRONT] Add update delivery modal component

--- ram-front/src/components/tables/SearchDeliveryRow.tsx
+++ ram-front/src/components/tables/SearchDeliveryRow.tsx
@@ -2,6 +2,8 @@ import { Checkbox, Table } from "flowbite-react";
 import { FiEdit, FiTrash2 } from "react-icons/fi";
 import { IDelivery } from "../../types";
 import { useUrlFile } from "../../lib/files";
+import ModalDeliveryFormUpdate from "../forms/ModalDeliveryFormUpdate";
+import useModal from "../../hooks/useModal";
 // (c) Delta Software 2023, rights reserved.
 
 export interface IListSalesProps {
@@ -10,6 +12,9 @@ export interface IListSalesProps {
 
 export const SearchDeliveryRow = ({ delivery }: IListSalesProps) => {
   const fileUrl = useUrlFile();
+
+  const { isOpen: isOpenDeliveryForm, toggleModal: toggleModalDeliveryForm } =
+    useModal();
 
   return (
     <>
@@ -28,7 +33,7 @@ export const SearchDeliveryRow = ({ delivery }: IListSalesProps) => {
         <Table.Cell>
           <button
             className="mr-2 cursor-pointer transition-all ease-in-out hover:scale-125"
-            onClick={() => alert("Redireccionando a editar delivery ...")}
+            onClick={toggleModalDeliveryForm}
           >
             <FiEdit
               color="gray"
@@ -36,6 +41,11 @@ export const SearchDeliveryRow = ({ delivery }: IListSalesProps) => {
               className="hover:stroke-gnp-blue-900"
             />
           </button>
+          <ModalDeliveryFormUpdate
+            isOpenModal={isOpenDeliveryForm}
+            closeModal={toggleModalDeliveryForm}
+            deliveryId={delivery.id}
+          />
           <button
             className="cursor-pointer transition-all ease-in-out hover:scale-125"
             onClick={() => alert("Redireccionando a eliminar delivery ...")}
