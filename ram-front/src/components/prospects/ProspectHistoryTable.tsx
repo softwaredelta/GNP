ff728949811252567c635dcac60863e08dd7f65d
Ren <56571410+Renato549@users.noreply.github.com>
2023-05-26 16:03:36 -0600

    247 front add tests prospect history (#247)
    Co-authored-by: Alfredo Huerta <74021668+alfredohuerta@users.noreply.github.com>

---
+++ ram-front/src/components/prospects/ProspectHistoryTable.tsx
@@ -0,0 +1,61 @@
+// (c) Delta Software 2023, rights reserved.
+
+import { IStatus } from "../../types";
+import ProspectListHistory from "./ProspectListHistory";
+
+type Props = {
+  History: IStatus[];
+};
+
+export default function ProspectsHistoryTable({ History }: Props) {
+  return (
+    <div className="flex flex-col items-center justify-center pt-8">
+      <>
+        <div
+          data-testid="prospect-list"
+          className="flex items-center justify-end"
+        >
+          <div className="m-6">
+            <div className="flex">
+              <div className="w-3/3 flex items-center pr-40">
+                <div className="rounded-md  bg-gnp-blue-500">
+                  <p className="px-5 py-2 text-center font-semibold text-gray-500 text-white">
+                    Estado
+                  </p>
+                </div>
+              </div>
+              <div className="w-3/3 px-19 flex items-center">
+                <div className="rounded-md  bg-gnp-blue-500">
+                  <p className="px-5 py-2 text-center font-semibold text-gray-500 text-white">
+                    Comentario
+                  </p>
+                </div>
+              </div>
+              <div className="w-3/3 flex items-center pl-36">
+                <div className="rounded-md  bg-gnp-blue-500">
+                  <p className="px-5 py-2 text-center font-semibold text-gray-500 text-white">
+                    Fecha de actualizacion de state
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="flex min-h-[26%] w-full justify-center gap-10">
+          <div className="w-3/5">
+            {History.map((h) => {
+              return (
+                <ProspectListHistory
+                  key={h.id}
+                  state={h.statusName}
+                  comment={h.comments}
+                  date={h.date}
+                />
+              );
+            })}
+          </div>
+        </div>
+      </>
+    </div>
+  );
+}
