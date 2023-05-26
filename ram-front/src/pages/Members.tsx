2aa146749d12a8668c47412fe6d70398ec002a25
Alejandro Martínez Luna <A01276785@tec.mx>
2023-05-25 22:08:58 -0600

    [FRONT] Modified Page Memebers for adding user

--- ram-front/src/pages/Members.tsx
+++ ram-front/src/pages/Members.tsx
@@ -1,32 +1,80 @@
 // (c) Delta Software 2023, rights reserved.
+import { useEffect } from "react";
+import Swal from "sweetalert2";
+import ModalAddUser from "../components/forms/ModalAddUser";
 import TableMembers from "../components/members/TableMembers";
 import Wrapper from "../containers/Wrapper";
 import useAxios from "../hooks/useAxios";
+import useModal from "../hooks/useModal";
 import { IMembers } from "../types";
 export default function Members() {
-  const { response, loading, error } = useAxios<IMembers[]>({
+  const {
+    response: membersResponse,
+    loading: loadingMembers,
+    error: errorMembers,
+  } = useAxios<IMembers[]>({
     url: "user/members",
     method: "GET",
   });
 
-  if (loading) return <h1>Loading ...</h1>;
-  if (error) return <h1>Error ...</h1>;
+  const {
+    response: createResponse,
+    loading: createLoading,
+    error: createError,
+    callback,
+  } = useAxios({
+    url: "user/create",
+    method: "POST",
+  });
+  useEffect(() => {
+    if (createResponse) {
+      Swal.fire({
+        title: "Usuario creado",
+        text: "El usuario se ha creado correctamente",
+        icon: "success",
+      });
+    }
+    if (createError) {
+      Swal.fire({
+        title: "Error",
+        text: `Ha ocurrido un error al crear el usuario.`,
+        icon: "error",
+      });
+    }
+  }, [createResponse, createError]);
+
+  const { isOpen, toggleModal } = useModal();
+
+  if (loadingMembers) return <h1>Loading ...</h1>;
+  if (errorMembers) return <h1>Error ...</h1>;
+
+  if (createLoading)
+    return (
+      <Wrapper>
+        <h1 className="title">Cargando...</h1>
+      </Wrapper>
+    );
 
   return (
     <Wrapper title="Miembros">
       <>
-        <div className="w-1/12 overflow-hidden rounded-3xl font-bold">
-          <button
-            onClick={() => {
-              alert("Agregar");
+        <div className="flex w-full items-center justify-end px-5 py-2">
+          <ModalAddUser
+            handlePost={(data) => {
+              if (callback) callback(data);
             }}
-            className="btn-primary"
-          >
-            Agregar
-          </button>
+            isOpenModal={isOpen}
+            closeModal={toggleModal}
+          />
+
+          <div className="w-1/12 overflow-hidden rounded-3xl font-bold">
+            <button onClick={toggleModal} className="btn-primary">
+              Agregar
+            </button>
+          </div>
         </div>
 
-        {response && <TableMembers members={response} />}
+        {membersResponse && <TableMembers members={membersResponse} />}
       </>
     </Wrapper>
   );
