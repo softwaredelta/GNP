f261200a751c553f8847175bb7617ae07632ceb5
Alejandro Martínez Luna <A01276785@tec.mx>
2023-05-28 04:26:29 -0600

    [BACK] Added function to modify prospect status

--- ram-back/src/app/prospect.ts
+++ ram-back/src/app/prospect.ts
@@ -1,9 +1,9 @@
 // (c) Delta Software 2023, rights reserved.
 
 import { getDataSource } from "../arch/db-client";
+import { ProspectStatusEnt } from "../entities/prospect-status.entity";
 import { ProspectEnt } from "../entities/prospect.entity";
 import { StatusEnt, StatusNames } from "../entities/status.entity";
-import { ProspectStatusEnt } from "../entities/prospect-status.entity";
 
 export enum ProspectError {
   PROSPECT_ERROR = "DEFAULT_ERROR",
@@ -138,6 +138,54 @@ export async function getProspectsByAgent(params: {
       error: ProspectError.PROSPECT_ERROR,
       reason: e as Error,
       prospects: [],
+    };
+  }
+}
+
+export async function modifyProspect(params: {
+  prospectId: string;
+  statusId?: string;
+  statusComment: string;
+}): Promise<{
+  prospect: ProspectEnt;
+  error?: ProspectError;
+  reason?: Error;
+}> {
+  const ds = await getDataSource();
+
+  const { prospectId, statusId, statusComment } = params;
+
+  try {
+    return await ds.manager.transaction(async (manager) => {
+      const prospect = await manager.findOne(ProspectEnt, {
+        where: { id: prospectId },
+        relations: { prospectStatus: { status: true } },
+      });
+
+      if (!prospect) {
+        throw new Error("Prospecto no encontrado");
+      }
+
+      await manager.save(
+        manager.create(ProspectStatusEnt, {
+          prospectId: prospect.id,
+          statusId,
+          statusComment,
+        }),
+      );
+
+      const prospectRelations = await manager.findOne(ProspectEnt, {
+        where: { id: prospect.id },
+        relations: { prospectStatus: { status: true } },
+      });
+
+      return { prospect: prospectRelations as ProspectEnt };
+    });
+  } catch (e) {
+    return {
+      prospect: {} as ProspectEnt,
+      error: e as ProspectError,
+      reason: new Error("No se pudo modificar el prospecto"),
     };
   }
 }
