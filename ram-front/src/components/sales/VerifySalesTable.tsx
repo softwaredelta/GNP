6fac22c16dea0340a4025596d42eaae1ed05cf04
Monica Ayala Marrero <a01707439@itesm.mx>
2023-05-29 03:08:20 -0600

    1808 add front filters to sales (#291)

--- ram-front/src/components/sales/VerifySalesTable.tsx
+++ ram-front/src/components/sales/VerifySalesTable.tsx
@@ -2,92 +2,165 @@
 
 import React, { useEffect, useState } from "react";
 
-import { Alert, Table } from "flowbite-react";
-import { AiFillWarning } from "react-icons/ai";
+import { Table } from "flowbite-react";
+import Pagination from "../generics/Pagination";
 import VerifySalesRow from "./VerifySalesRow";
-
-export interface AssuranceType {
-  id: string;
-  name: string;
-  description: string;
-}
-
-export interface User {
-  id: string;
-  name: string;
-  lastName: string;
-  email: string;
-}
+import { ISell } from "../../types";
+import SalesFilters from "./SalesFilters";
+import { Column, usePagination, useTable } from "react-table";
+import { NumericFormat } from "react-number-format";
 export interface IListSalesProps {
-  sales: {
-    id: string;
-    policyNumber: string;
-    assuranceType: AssuranceType;
-    paidDate: Date;
-    yearlyFee: string;
-    contractingClient: string;
-    status: string;
-    periodicity: string;
-    user: User;
-    evidenceUrl: string;
-  }[];
+  sales: ISell[];
   onUpdated?: () => void;
 }
 
+const columns: Column<ISell>[] = [
+  {
+    Header: "Nombre del Cliente",
+    accessor: "user",
+  },
+  {
+    Header: "Póliza",
+    accessor: "policyNumber",
+  },
+  {
+    Header: "Prima Anual",
+    accessor: "yearlyFee",
+  },
+  {
+    Header: "Prima Pagada",
+    accessor: "paidFee",
+  },
+  {
+    Header: "Periocidad",
+    accessor: "periodicity",
+  },
+
+  {
+    Header: "Tipo de Seguro",
+    accessor: "assuranceType",
+  },
+  {
+    Header: "Fecha Emisión",
+    accessor: "emissionDate",
+  },
+  {
+    Header: "Fecha Pago",
+    accessor: "paidDate",
+  },
+  {
+    Header: "Estado",
+    accessor: "status",
+  },
+];
+
 export const VerifySalesTable = ({ sales }: IListSalesProps) => {
+  const [handleUpdate, setHandleUpdate] = useState<boolean>(false);
+
+  const [data, setData] = useState<ISell[]>(sales);
+  const {
+    page,
+    pageOptions,
+    canPreviousPage,
+    canNextPage,
+    previousPage,
+    nextPage,
+    state,
+  } = useTable(
+    {
+      columns,
+      data,
+      initialState: { pageIndex: 0, pageSize: 10 },
+    },
+    usePagination,
+  );
+
   const [shouldUpdate, setShouldUpdate] = useState<boolean>(false);
   useEffect(() => {
     if (shouldUpdate) {
-      setShouldUpdate(false);
+      setHandleUpdate(false);
+      if (handleUpdate) setShouldUpdate(false);
     }
   }, [shouldUpdate]);
 
   return (
     <div data-testid="Table" className="w-full p-8">
-      <Table hoverable={true}>
+      <div>
+        <SalesFilters sales={sales} setSales={setData} />
+      </div>
+      <Table className="row" hoverable={true}>
         <Table.Head>
-          <Table.HeadCell>Nombre del agente</Table.HeadCell>
-          <Table.HeadCell>Nombre del Cliente</Table.HeadCell>
-          <Table.HeadCell>Póliza</Table.HeadCell>
-          <Table.HeadCell>Monto</Table.HeadCell>
-          <Table.HeadCell>Tipo de Seguro</Table.HeadCell>
-          <Table.HeadCell>Fecha</Table.HeadCell>
-          <Table.HeadCell>Estado</Table.HeadCell>
-          <Table.HeadCell> Acciones </Table.HeadCell>
+          {columns.map((column) => {
+            return (
+              <Table.HeadCell key={column.accessor as string}>
+                {column.Header as string}
+              </Table.HeadCell>
+            );
+          })}
+          <Table.HeadCell>
+            <span className="sr-only">Edit</span>
+          </Table.HeadCell>
         </Table.Head>
         <Table.Body className="divide-y">
-          {sales.length === 0 && (
-            <div className="flex items-center justify-center">
-              <Alert
-                color="info"
-                icon={AiFillWarning}
-                className="ml-10 w-full text-center font-bold"
-              >
-                <h1 className="text-base">No hay ventas para verificar</h1>
-              </Alert>
-            </div>
-          )}
-          {sales.map((sale) => {
+          {page.map((sale) => {
             return (
               <VerifySalesRow
                 key={sale.id}
-                id={sale.id}
-                agentName={sale.user.name + " " + sale.user.lastName}
-                contractingClient={sale.contractingClient}
-                yearlyFee={sale.yearlyFee}
-                assuranceTypeName={sale.assuranceType.name}
-                paidDate={sale.paidDate}
-                status={sale.status}
-                policyNum={sale.policyNumber}
-                evidenceUrl={sale.evidenceUrl}
+                sale={sale.original}
                 onUpdated={() => {
-                  setShouldUpdate(true);
+                  setHandleUpdate(true);
                 }}
               />
             );
           })}
+          <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+            <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+              {"Total"}
+            </Table.Cell>
+            <Table.Cell>{""}</Table.Cell>
+            <Table.Cell>
+              <NumericFormat
+                value={data
+                  .map((sale) => sale.yearlyFee)
+                  .reduce((a, b) => Number(a) + Number(b), 0)}
+                displayType={"text"}
+                thousandSeparator={true}
+                decimalScale={2}
+                decimalSeparator="."
+                fixedDecimalScale={true}
+                prefix={"$"}
+              />
+            </Table.Cell>
+            <Table.Cell>
+              <NumericFormat
+                value={data
+                  .map((sale) => sale.paidFee)
+                  .reduce((a, b) => Number(a) + Number(b), 0)}
+                displayType={"text"}
+                thousandSeparator={true}
+                decimalScale={2}
+                decimalSeparator="."
+                fixedDecimalScale={true}
+                prefix={"$"}
+              />{" "}
+            </Table.Cell>
+            <Table.Cell>{""}</Table.Cell>
+            <Table.Cell>{""}</Table.Cell>
+            <Table.Cell>{""}</Table.Cell>
+            <Table.Cell>{""}</Table.Cell>
+            <Table.Cell>{""}</Table.Cell>
+            <Table.Cell>{""}</Table.Cell>
+          </Table.Row>
         </Table.Body>
       </Table>
+      <Pagination
+        lastPage={pageOptions.length}
+        pageCurrent={state.pageIndex + 1}
+        canNextPage={canNextPage}
+        canPreviousPage={canPreviousPage}
+        nextPage={nextPage}
+        previousPage={previousPage}
+      />
     </div>
   );
 };
