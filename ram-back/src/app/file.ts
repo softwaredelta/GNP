90a0573f9d57cce7fd5b11a8dd347d9da03fe4df
Ren <A01275401@itesm.mx>
2023-06-03 18:49:30 -0600

    Documented app/assuranceType.ts, auth.ts, deliveries.ts, file.ts, groups.ts

--- ram-back/src/app/file.ts
+++ ram-back/src/app/file.ts
@@ -3,6 +3,13 @@ import { S3Api, getS3Api } from "../arch/s3-client";
 import path from "path";
 import { v4 as uuid } from "uuid";
 
+/**
+ * This function uploads a file to an S3 bucket and returns the filename.
+ * @param params - The function `uploadFile` takes in an object `params` as its parameter, which has
+ * one property `file`. The `file` property is of type `Express.Multer.File`, which is an interface for
+ * files uploaded using the Multer middleware in an Express.js application.
+ * @returns a string, which is the filename of the uploaded file.
+ */
 export async function uploadFile(params: {
   file: Express.Multer.File;
 }): Promise<string> {
