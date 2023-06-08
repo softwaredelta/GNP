e8676669a175afb3da8edd0369cb72747db3ae19
JordanaBM <A01707434@itesm.mx>
2023-06-07 18:21:57 -0600

    [FRONT] Changed name to groups

--- ram-front/src/pages/Metrics.tsx
+++ ram-front/src/pages/Metrics.tsx
@@ -70,7 +70,7 @@ export default function Metrics(): JSX.Element {
         <div className="relative w-full rounded-xl bg-slate-200 p-12">
           <div className="absolute -top-5 left-0 right-0 col-span-1 grid justify-center">
             <h1 className="rounded-3xl border border-solid border-slate-600 bg-slate-100 p-2 px-8 text-center text-2xl font-bold">
-              Cursos
+              Grupos
             </h1>
           </div>
           {dataGroups && (
